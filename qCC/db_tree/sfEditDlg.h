3827915f69caafa46d5c1a813cefb8796feacca6
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:16:55 +0100

    docs: Add Doxygen comments to sfEditDlg.h
    - Add file-level documentation
    - Document sfEditDlg class
    
    Scalar field edit dialog.

--- qCC/db_tree/sfEditDlg.h
+++ qCC/db_tree/sfEditDlg.h
@@ -18,6 +18,16 @@
 #ifndef CC_SF_EDIT_DIALOG_HEADER
 #define CC_SF_EDIT_DIALOG_HEADER
 
+/**
+ * @file sfEditDlg.h
+ *
+ * @brief Scalar field edit dialog
+ *
+ * Widget for editing scalar field display parameters.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
+
 // Qt
 #include <QWidget>
 
@@ -29,51 +39,77 @@ namespace Ui
 	class SFEditDlg;
 }
 
-//! GUI scalar field interactor for properties list dialog
+/**
+ * @brief Scalar field edit dialog
+ *
+ * Scalar field display parameters editor.
+ */
 class sfEditDlg : public QWidget
 {
 	Q_OBJECT
 
   public:
-	//! Default constructor
+	/**
+	 * @brief Create widget
+	 * @param[in] parent Parent widget
+	 */
 	explicit sfEditDlg(QWidget* parent = nullptr);
 
+	/// Destructor
 	~sfEditDlg();
 
-	//! Updates dialog with a given scalar field
+	/**
+	 * @brief Fill with scalar field
+	 * @param[in] sf Scalar field
+	 */
 	void fillDialogWith(ccScalarField* sf);
 
   public:
+	/// Handle min value spinbox changed
 	void minValSBChanged(double);
+	/// Handle max value spinbox changed
 	void maxValSBChanged(double);
+	/// Handle min saturation spinbox changed
 	void minSatSBChanged(double);
+	/// Handle max saturation spinbox changed
 	void maxSatSBChanged(double);
 
+	/// Handle min value from histogram changed
 	void minValHistoChanged(double);
+	/// Handle max value from histogram changed
 	void maxValHistoChanged(double);
+	/// Handle min saturation from histogram changed
 	void minSatHistoChanged(double);
+	/// Handle max saturation from histogram changed
 	void maxSatHistoChanged(double);
 
+	/// Handle NaN in gray changed
 	void nanInGrayChanged(bool);
+	/// Handle always show 0 changed
 	void alwaysShow0Changed(bool);
+	/// Handle symmetrical scale changed
 	void symmetricalScaleChanged(bool);
+	/// Handle log scale changed
 	void logScaleChanged(bool);
 
   Q_SIGNALS:
 
-	//! Signal emitted when the SF display parameters have changed
+	/// Emitted when SF display parameters change
 	void entitySFHasChanged();
 
   protected:
-	// conversionb between sliders (integer) and checkbox (double) values
+	/// Convert spinbox to slider value
 	double dispSpin2slider(double val) const;
+	/// Convert saturation spinbox to slider value
 	double satSpin2slider(double val) const;
+	/// Convert slider to spinbox value
 	double dispSlider2spin(int pos) const;
+	/// Convert saturation slider to spinbox value
 	double satSlider2spin(int pos) const;
 
-	//! Associated scalar field
+	/// Associated scalar field
 	ccScalarField* m_associatedSF;
-	//! Associated scalar field histogram
+	/// Associated histogram
 	ccHistogramWindow* m_associatedSFHisto;
 
 	Ui::SFEditDlg* m_ui;
