2228a30515506b93c011c3f30a2f781f2996dc72
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:33:52 +0100

    docs: Add Doxygen comments to ccDefaultPluginInterface.h
    - Add file-level documentation
    - Document ccDefaultPluginInterface class
    - Document plugin lifecycle methods
    
    Default plugin interface base class.

--- libs/CCPluginStub/include/ccDefaultPluginInterface.h
+++ libs/CCPluginStub/include/ccDefaultPluginInterface.h
@@ -17,52 +17,94 @@
 
 #pragma once
 
+/**
+ * @file ccDefaultPluginInterface.h
+ *
+ * @brief Default plugin interface base class
+ *
+ * Base class for CloudCompare plugins with default
+ * implementations for common plugin methods.
+ *
+ * @author CloudCompare project
+ */
+
 #include "ccPluginInterface.h"
 
 #include <QString>
 
 class ccDefaultPluginData;
 
+/**
+ * @brief Default plugin interface
+ *
+ * Base class for plugins with default implementations.
+ */
 class ccDefaultPluginInterface : public ccPluginInterface
 {
   public:
+	/**
+	 * @brief Destructor
+	 */
 	~ccDefaultPluginInterface() override;
 
+	/// Check if this is a core plugin
 	bool isCore() const override;
 
+	/// Get plugin name
 	QString getName() const override;
+	
+	/// Get plugin description
 	QString getDescription() const override;
 
+	/// Get plugin icon
 	QIcon getIcon() const override;
 
+	/// Get references
 	ReferenceList getReferences() const override;
-	ContactList   getAuthors() const override;
-	ContactList   getMaintainers() const override;
+	
+	/// Get authors
+	ContactList getAuthors() const override;
+	
+	/// Get maintainers
+	ContactList getMaintainers() const override;
 
+	/// Start the plugin
 	bool start() override
 	{
 		return true;
 	}
+	
+	/// Stop the plugin
 	void stop() override
 	{
 	}
 
+	/// Get custom objects factory
 	ccExternalFactory* getCustomObjectsFactory() const override
 	{
 		return nullptr;
 	}
 
+	/// Register command line commands
 	void registerCommands(ccCommandLineInterface* cmd) override
 	{
 		Q_UNUSED(cmd);
 	}
 
   protected:
+	/**
+	 * @brief Create plugin
+	 * @param[in] resourcePath Path to resources
+	 */
 	ccDefaultPluginInterface(const QString& resourcePath = QString());
 
   private:
-	void           setIID(const QString& iid) override;
+	/// Set plugin IID
+	void setIID(const QString& iid) override;
+	
+	/// Get plugin IID
 	const QString& IID() const override;
 
+	/// Plugin data
 	ccDefaultPluginData* m_data;
 };
