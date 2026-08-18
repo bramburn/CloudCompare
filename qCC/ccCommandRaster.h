6dd386e5bd0c65ee53e9d696356fe62d698fd21f
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:53:35 +0100

    docs: Add Doxygen comments to ccCommandRaster.h
    - Add file-level documentation
    - Document CommandRasterize, CommandVolume25D
    
    Raster commands.

--- qCC/ccCommandRaster.h
+++ qCC/ccCommandRaster.h
@@ -1,19 +1,35 @@
 #ifndef COMMAND_LINE_RASTER_HEADER
 #define COMMAND_LINE_RASTER_HEADER
 
+/**
+ * @file ccCommandRaster.h
+ *
+ * @brief Raster commands
+ *
+ * Command line raster processing commands.
+ *
+ * @author CloudCompare project
+ */
+
 #include "ccCommandLineInterface.h"
 
+/// Rasterize command
 struct CommandRasterize : public ccCommandLineInterface::Command
 {
+	/// Constructor
 	CommandRasterize();
 
+	/// Process command
 	bool process(ccCommandLineInterface& cmd) override;
 };
 
+/// 2.5D volume command
 struct CommandVolume25D : public ccCommandLineInterface::Command
 {
+	/// Constructor
 	CommandVolume25D();
 
+	/// Process command
 	bool process(ccCommandLineInterface& cmd) override;
 };
 
