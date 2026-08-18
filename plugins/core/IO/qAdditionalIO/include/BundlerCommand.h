42b1fd29278d630a7aa9e727ebf2f353575c353d
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:32:36 +0100

    docs: Add Doxygen comments to BundlerCommand.h
    - Add file-level documentation
    - Document BundlerCommand class
    
    Bundler command.

--- plugins/core/IO/qAdditionalIO/include/BundlerCommand.h
+++ plugins/core/IO/qAdditionalIO/include/BundlerCommand.h
@@ -7,7 +7,7 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.     #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -18,16 +18,35 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file BundlerCommand.h
+ *
+ * @brief Bundler command
+ *
+ * Command line interface command for Bundler import.
+ *
+ * @author CloudCompare project
+ */
+
 #include "ccCommandLineInterface.h"
 
+/**
+ * @brief Bundler command
+ *
+ * CLI command for importing Bundler files.
+ */
 class BundlerCommand : public ccCommandLineInterface::Command
 {
   public:
+	/// Constructor
 	BundlerCommand();
+	
+	/// Destructor
 	virtual ~BundlerCommand()
 	{
 	}
 
+	/// Process command
 	virtual bool process(ccCommandLineInterface& cmd) override;
 };
 
