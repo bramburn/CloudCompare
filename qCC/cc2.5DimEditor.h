dbb89ffb802469c07e89c5058f5d8e1e20c74d72
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:49:41 +0100

    docs: Add Doxygen comments to cc2.5DimEditor.h
    - Add file-level documentation
    - Document cc2Point5DimEditor class
    
    2.5D data editor interface.

--- qCC/cc2.5DimEditor.h
+++ qCC/cc2.5DimEditor.h
@@ -18,6 +18,16 @@
 #ifndef CC_2_5D_EDITOR_HEADER
 #define CC_2_5D_EDITOR_HEADER
 
+/**
+ * @file cc2.5DimEditor.h
+ *
+ * @brief 2.5D data editor
+ *
+ * Generic interface for 2.5D raster data editing.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
+
 // qCC_db
 #include <ccRasterGrid.h>
 
@@ -27,84 +37,54 @@ class QWidget;
 class QFrame;
 class QComboBox;
 
-//! 2.5D data editor (generic interface)
+/**
+ * @brief 2.5D editor interface
+ *
+ * Generic interface for 2.5D raster data editing.
+ */
 class cc2Point5DimEditor
 {
   public:
-	//! Default constructor
+	/// Default constructor
 	cc2Point5DimEditor();
 
-	//! Destructor
+	/// Destructor
 	virtual ~cc2Point5DimEditor();
 
   protected: // standard methods
-	//! Returns projection grid step
+	/// Get projection grid step
 	virtual double getGridStep() const = 0;
 
-	//! Returns projection dimension
-	/** \return dimension as int (0: X, 1: Y, 2:Z)
-	 **/
+	/// Get projection dimension (0=X, 1=Y, 2=Z)
 	virtual unsigned char getProjectionDimension() const = 0;
 
-	//! Returns type of projection
+	/// Get projection type
 	virtual ccRasterGrid::ProjectionType getTypeOfProjection() const = 0;
 
-	//! Returns custom bbox
+	/// Get custom bbox
 	virtual ccBBox getCustomBBox() const;
 
-	//! Declares whether the grid is up-to-date or not
+	/// Grid is up-to-date
 	virtual void gridIsUpToDate(bool state) = 0;
 
-	//! Updates the 2D display zoom
+	/// Update 2D display zoom
 	virtual void update2DDisplayZoom(ccBBox& box);
 
   protected: // raster grid related stuff
-	//! Show grid box editor and update
-	/** \return whether the box was modified or not
-	 **/
+	/// Show grid box editor
 	virtual bool showGridBoxEditor();
 
-	//! Returns the grid size as a string
+	/// Get grid size as string
 	virtual QString getGridSizeAsString() const;
 
-	//! Returns the grid size
+	/// Get grid size
 	virtual bool getGridSize(unsigned& width, unsigned& height) const;
 
-	//! Creates the bounding-box editor
+	/// Create bounding box editor
 	void createBoundingBoxEditor(const ccBBox& gridBBox, QWidget* parent);
 
-	//! Creates the 2D view
+	/// Create 2D view
 	void create2DView(QFrame* parentFrame);
 
-	//! Returns the empty cell strategy (for a given combo-box)
+	/// Get fill empty cells strategy
 	ccRasterGrid::EmptyCellFillOption getFillEmptyCellsStrategy(QComboBox* comboBox) const;
-
-	//! Shortcut to ccRasterGrid::convertToCloud
-	ccPointCloud* convertGridToCloud(bool                                               exportHeightStats,
-	                                 bool                                               exportSFStats,
-	                                 const std::vector<ccRasterGrid::ExportableFields>& exportedStatistics,
-	                                 bool                                               projectSFs,
-	                                 bool                                               projectColors,
-	                                 bool                                               resampleInputCloudXY,
-	                                 bool                                               resampleInputCloudZ, // only considered if resampleInputCloudXY is true!
-	                                 ccGenericPointCloud*                               inputCloud,
-	                                 double                                             percentileValue,
-	                                 bool                                               exportToOriginalCS,
-	                                 bool                                               appendGridSizeToSFNames,
-	                                 ccProgressDialog*                                  progressDialog = nullptr) const;
-
-  protected: // members
-	//! Raster grid
-	ccRasterGrid m_grid;
-
-	//! Associated dialog
-	ccBoundingBoxEditorDlg* m_bbEditorDlg;
-
-	//! 2D display
-	ccGLWindowInterface* m_glWindow;
-
-	//! 'Raster' cloud
-	ccPointCloud* m_rasterCloud;
-};
-
-#endif // CC_2_5D_EDITOR_HEADER
