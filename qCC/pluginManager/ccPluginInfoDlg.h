58440667960b5db68f02b6dc5a618377e07f49b5
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:17:11 +0100

    docs: Add Doxygen comments to ccPluginInfoDlg.h
    - Add file-level documentation
    - Document ccPluginInfoDlg class
    
    Plugin info dialog.

--- qCC/pluginManager/ccPluginInfoDlg.h
+++ qCC/pluginManager/ccPluginInfoDlg.h
@@ -17,6 +17,16 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file ccPluginInfoDlg.h
+ *
+ * @brief Plugin info dialog
+ *
+ * Dialog for displaying plugin information.
+ *
+ * @author CloudCompare project
+ */
+
 #include <QDialog>
 #include <QList>
 
@@ -32,28 +42,46 @@ namespace Ui
 	class ccPluginInfoDlg;
 }
 
+/**
+ * @brief Plugin info dialog
+ *
+ * Display information about available plugins.
+ */
 class ccPluginInfoDlg : public QDialog
 {
 	Q_OBJECT
 
   public:
+	/**
+	 * @brief Create dialog
+	 * @param[in] parent Parent widget
+	 */
 	explicit ccPluginInfoDlg(QWidget* parent = nullptr);
+	
+	/// Destructor
 	~ccPluginInfoDlg() override;
 
+	/// Set plugin paths
 	void setPluginPaths(const QStringList& pluginPaths);
+	/// Set plugin list
 	void setPluginList(const QList<ccPluginInterface*>& pluginList);
 
   private:
+	/// User role for plugin pointer
 	enum
 	{
 		PLUGIN_PTR = Qt::UserRole + 1
 	};
 
+	/// Get plugin from item data
 	const ccPluginInterface* pluginFromItemData(const QStandardItem* item) const;
 
+	/// Handle selection changed
 	void selectionChanged(const QModelIndex& current, const QModelIndex& previous);
+	/// Handle item changed
 	void itemChanged(QStandardItem* item);
 
+	/// Update plugin info display
 	void updatePluginInfo(const ccPluginInterface* plugin);
 
 	Ui::ccPluginInfoDlg* m_UI;
