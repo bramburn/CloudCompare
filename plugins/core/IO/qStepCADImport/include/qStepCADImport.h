5e55695dd3c50941cc12912cd1d95a46b19a4949
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 22:01:26 +0100

    docs: Add Doxygen comments to qStepCADImport.h
    - Add file-level documentation
    - Document qStepCADImport plugin class
    
    STEP CAD import plugin.

--- plugins/core/IO/qStepCADImport/include/qStepCADImport.h
+++ plugins/core/IO/qStepCADImport/include/qStepCADImport.h
@@ -6,20 +6,34 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                          COPYRIGHT: EDF R&D                            #
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file qStepCADImport.h
+ *
+ * @brief STEP CAD import plugin
+ *
+ * Plugin for importing STEP CAD files.
+ *
+ * @author EDF R&D
+ */
+
 #include <ccIOPluginInterface.h>
 
-//! Step file import plugin
+/**
+ * @brief STEP CAD import plugin
+ *
+ * Import STEP CAD files.
+ */
 class qStepCADImport : public QObject
     , public ccIOPluginInterface
 {
@@ -29,10 +43,15 @@ class qStepCADImport : public QObject
 	Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.qStepCADImport" FILE "../info.json")
 
   public:
-	//! Default constructor
+	/**
+	 * @brief Create plugin
+	 * @param[in] parent Parent object
+	 */
 	explicit qStepCADImport(QObject* parent = nullptr);
 
-	// inherited from ccIOPluginInterface
+	/// Register command line commands
 	void       registerCommands(ccCommandLineInterface* inCmdLine) override;
+	
+	/// Get list of supported filters
 	FilterList getFilters() override;
 };
