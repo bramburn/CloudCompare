0dea0e01e049dc7777db098202198a68504c87ca
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:58:01 +0100

    docs: Add Doxygen comments to RDBOpenDialog.h
    - Add file-level documentation
    - Document RDBOpenDialog class
    
    RDB open dialog.

--- plugins/core/IO/qRDBIO/include/RDBOpenDialog.h
+++ plugins/core/IO/qRDBIO/include/RDBOpenDialog.h
@@ -4,7 +4,7 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -18,12 +18,27 @@
 #ifndef RDB_OPEN_DIALOG_HEADER
 #define RDB_OPEN_DIALOG_HEADER
 
+/**
+ * @file RDBOpenDialog.h
+ *
+ * @brief RDB open dialog
+ *
+ * Dialog for opening RDB files.
+ *
+ * @author RIEGL Laser Measurement Systems GmbH
+ */
+
 // Qt
 #include <QDialog>
 
 // GUI
 #include "ui_openRDBDlg.h"
 
+/**
+ * @enum CC_RDB_OPEN_DLG_TYPES
+ *
+ * @brief RDB open dialog field types
+ */
 enum CC_RDB_OPEN_DLG_TYPES
 {
 	RDB_OPEN_DLG_None   = 0,
@@ -33,6 +48,7 @@ enum CC_RDB_OPEN_DLG_TYPES
 	RDB_OPEN_DLG_Grey   = 4,
 	RDB_OPEN_DLG_Scalar = 5,
 };
+
 const unsigned RDB_OPEN_DLG_TYPES_NUMBER                               = 6;
 const char     RDB_OPEN_DLG_TYPES_NAMES[RDB_OPEN_DLG_TYPES_NUMBER][24] = {
     "Ignore",
@@ -42,14 +58,24 @@ const char     RDB_OPEN_DLG_TYPES_NAMES[RDB_OPEN_DLG_TYPES_NUMBER][24] = {
     "Grey",
     "Scalar",
 };
-//! RDB Open dialog
+
+/**
+ * @class RDBOpenDialog
+ *
+ * @brief RDB open dialog
+ *
+ * Configure RDB file import options.
+ */
 class RDBOpenDialog : public QDialog
     , public Ui::RDBOpenDlg
 {
 	Q_OBJECT
 
   public:
-	//! Default constructor
+	/**
+	 * @brief Create dialog
+	 * @param[in] parent Parent widget
+	 */
 	explicit RDBOpenDialog(QWidget* parent = nullptr);
 };
 
