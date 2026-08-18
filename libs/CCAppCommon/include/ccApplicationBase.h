f0cb3fb74ed24496881ae1f0b7be75ff53651074
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:34:49 +0100

    docs: Add Doxygen comments to ccApplicationBase.h
    - Add file-level documentation
    - Document ccApplicationBase class
    - Document InitOpenGL, version methods
    
    Application base class.

--- libs/CCAppCommon/include/ccApplicationBase.h
+++ libs/CCAppCommon/include/ccApplicationBase.h
@@ -16,48 +16,105 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file ccApplicationBase.h
+ *
+ * @brief Application base class
+ *
+ * Base application class for CloudCompare.
+ *
+ * @author CloudCompare project
+ */
+
 #include "CCAppCommon.h"
 
 // Qt
 #include <QApplication>
 
-//! Mimic Qt's qApp for easy access to the application instance
+/// Global application instance accessor
 #define ccApp (static_cast<ccApplicationBase*>(QCoreApplication::instance()))
 
+/**
+ * @brief Application base class
+ *
+ * Base class for CloudCompare application.
+ */
 class CCAPPCOMMON_LIB_API ccApplicationBase : public QApplication
 {
   public:
-	//! This must be called before instantiating the application class so it
-	//! can setup OpenGL first.
+	/**
+	 * @brief Initialize OpenGL
+	 *
+	 * Must be called before instantiating the application class.
+	 */
 	static void InitOpenGL();
 
+	/**
+	 * @brief Create application
+	 * @param[in] argc Argument count
+	 * @param[in] argv Argument values
+	 * @param[in] isCommandLine Command line mode
+	 * @param[in] version Version string
+	 */
 	ccApplicationBase(int& argc, char** argv, bool isCommandLine, const QString& version);
 
+	/**
+	 * @brief Check if command line mode
+	 * @return true if command line
+	 */
 	inline bool isCommandLine() const
 	{
 		return m_isCommandLine;
 	}
+	
+	/**
+	 * @brief Get translation path
+	 * @return Path to translations
+	 */
 	inline const QString& translationPath() const
 	{
 		return m_translationPath;
 	}
+	
+	/**
+	 * @brief Get version string
+	 * @return Version
+	 */
 	inline const QString& versionStr() const
 	{
 		return m_versionStr;
 	}
+	
+	/**
+	 * @brief Get full version string
+	 * @param[in] includeOS Include OS info
+	 * @return Full version string
+	 */
 	QString versionLongStr(bool includeOS) const;
 
-	//! Set the application style (based on a QStyleFactory key)
+	/**
+	 * @brief Set application style
+	 * @param[in] styleKey QStyleFactory key
+	 * @return true on success
+	 */
 	bool setAppStyle(QString styleKey);
 
   private:
+	/// Setup paths
 	void setupPaths();
 
+	/// Version string
 	const QString m_versionStr;
 
-	QString     m_shaderPath;
-	QString     m_translationPath;
+	/// Shader path
+	QString m_shaderPath;
+	
+	/// Translation path
+	QString m_translationPath;
+	
+	/// Plugin paths
 	QStringList m_pluginPaths;
 
+	/// Command line mode flag
 	const bool m_isCommandLine;
 };
