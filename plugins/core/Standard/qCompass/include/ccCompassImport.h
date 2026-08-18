9be0689581ad7e2e1f063451e439c8a14fbfbf49
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 22:31:19 +0100

    docs: Add Doxygen comments to ccCompassImport.h
    - Add file and namespace documentation
    - Document ccCompassImport namespace
    
    Compass import.

--- plugins/core/Standard/qCompass/include/ccCompassImport.h
+++ plugins/core/Standard/qCompass/include/ccCompassImport.h
@@ -1,15 +1,34 @@
 #ifndef CCCOMPASSIMPORT_H
 #define CCCOMPASSIMPORT_H
 
+/**
+ * @file ccCompassImport.h
+ *
+ * @brief Compass import functions
+ *
+ * Import foliation and lineation data.
+ */
+
 class QString;
 
 class ccMainAppInterface;
 
-
+/**
+ * @brief Compass import functions
+ */
 namespace ccCompassImport
 {
-	void importFoliations( ccMainAppInterface *app ); //import foliation data
-	void importLineations( ccMainAppInterface *app ); //import lineation data
+	/**
+	 * @brief Import foliation data
+	 * @param[in] app Main application interface
+	 */
+	void importFoliations( ccMainAppInterface *app );
+	
+	/**
+	 * @brief Import lineation data
+	 * @param[in] app Main application interface
+	 */
+	void importLineations( ccMainAppInterface *app );
 };
 
 #endif // CCCOMPASSIMPORT_H
