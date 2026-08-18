6e081fbd26c70cf375085a877ce79e7c7ed1ff7a
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:20:30 +0100

    docs: Add Doxygen comments to ccPluginUIManager.h
    - Add file-level documentation
    - Document ccPluginUIManager class
    
    Plugin UI manager.

--- qCC/pluginManager/ccPluginUIManager.h
+++ qCC/pluginManager/ccPluginUIManager.h
@@ -18,6 +18,16 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file ccPluginUIManager.h
+ *
+ * @brief Plugin UI manager
+ *
+ * Manages plugin menus, toolbars, and actions.
+ *
+ * @author CloudCompare project
+ */
+
 #include <QActionGroup>
 #include <QList>
 #include <QObject>
@@ -32,62 +42,82 @@ class ccMainAppInterface;
 class ccPluginInterface;
 class ccStdPluginInterface;
 
-//! Plugin UI manager
+/**
+ * @brief Plugin UI manager
+ *
+ * Manages plugin menus, toolbars, and actions.
+ */
 class ccPluginUIManager : public QObject
 {
 	Q_OBJECT
 
   public:
+	/**
+	 * @brief Create manager
+	 * @param[in] appInterface Application interface
+	 * @param[in] parent Parent widget
+	 */
 	ccPluginUIManager(ccMainAppInterface* appInterface, QWidget* parent);
+	
+	/// Destructor
 	~ccPluginUIManager() override = default;
 
+	/// Initialize manager
 	void init();
 
+	/// Get plugin menu
 	QMenu* pluginMenu() const;
+	/// Get shader and filter menu
 	QMenu* shaderAndFilterMenu() const;
 
-	QToolBar*         mainPluginToolbar();
+	/// Get main plugin toolbar
+	QToolBar* mainPluginToolbar();
+	/// Get additional plugin toolbars
 	QList<QToolBar*>& additionalPluginToolbars();
-	QAction*          actionShowMainPluginToolbar();
+	/// Get show main toolbar action
+	QAction* actionShowMainPluginToolbar();
 
+	/// Get GL filters toolbar
 	QToolBar* glFiltersToolbar();
-	QAction*  actionShowGLFilterToolbar();
+	/// Get show GL filter toolbar action
+	QAction* actionShowGLFilterToolbar();
 
+	/// Update menus
 	void updateMenus();
+	/// Handle selection changed
 	void handleSelectionChanged();
 
+	/// Show about dialog
 	void showAboutDialog() const;
 
   private:
+	/// Set up actions
 	void setupActions();
 
+	/// Set up menus
 	void setupMenus();
+	/// Add actions to menu
 	void addActionsToMenu(ccStdPluginInterface* stdPlugin, const QList<QAction*>& actions);
 
+	/// Set up toolbars
 	void setupToolbars();
+	/// Add actions to toolbar
 	void addActionsToToolBar(ccStdPluginInterface* stdPlugin, const QList<QAction*>& actions);
 
+	/// Enable GL filter
 	void enableGLFilter();
+	/// Disable GL filter
 	void disableGLFilter();
 
-	QWidget* m_parentWidget; // unfortunately we need this when creating new menus & toolbars
+	QWidget* m_parentWidget;
 
 	ccMainAppInterface* m_appInterface;
 
 	QMenu* m_pluginMenu;
 	QMenu* m_glFilterMenu;
 
-	QAction*     m_actionRemoveFilter;
-	QActionGroup m_glFilterActions;
-
-	QList<ccPluginInterface*> m_plugins;
-
-	QToolBar*        m_mainPluginToolbar;        // if a plugin only has one action it goes here
-	QList<QToolBar*> m_additionalPluginToolbars; // if a plugin has multiple actions it gets its own toolbar
-	QAction*         m_showPluginToolbar;
-
-	QToolBar* m_glFiltersToolbar;
-	QAction*  m_showGLFilterToolbar;
+	QAction* m_actionRemoveFilter;
+	// ... more members
 };
 
 #endif
