ada9b2eb48086fe9f5d779be14f4025e7449a629
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:28:50 +0100

    docs: Add Doxygen comments to ccPluginManager.h
    - Add file-level documentation
    - Document ccPluginManager class
    - Document plugin loading and lifecycle methods
    
    Plugin manager singleton.

--- libs/CCAppCommon/include/ccPluginManager.h
+++ libs/CCAppCommon/include/ccPluginManager.h
@@ -16,6 +16,16 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file ccPluginManager.h
+ *
+ * @brief Plugin manager for CloudCompare
+ *
+ * Manages loading and lifecycle of plugins.
+ *
+ * @author CloudCompare project
+ */
+
 #include "CCAppCommon.h"
 
 #include <QObject>
@@ -23,38 +33,84 @@
 
 class ccPluginInterface;
 
-//! Simply a list of \see ccPluginInterface
+/// List of plugin interfaces
 using ccPluginInterfaceList = QVector<ccPluginInterface*>;
 
-//! Plugin manager
+/**
+ * @brief Plugin manager singleton
+ *
+ * Manages plugin loading and lifecycle.
+ */
 class CCAPPCOMMON_LIB_API ccPluginManager : public QObject
 {
 	Q_OBJECT
 
   public:
+	/**
+	 * @brief Destructor
+	 */
 	~ccPluginManager() override = default;
 
+	/**
+	 * @brief Get singleton instance
+	 * @return Plugin manager instance
+	 */
 	static ccPluginManager& Get();
 
-	void        setPaths(const QStringList& paths);
+	/**
+	 * @brief Set plugin search paths
+	 * @param[in] paths Directories to search for plugins
+	 */
+	void setPaths(const QStringList& paths);
+	
+	/**
+	 * @brief Get plugin search paths
+	 * @return List of paths
+	 */
 	QStringList pluginPaths() const;
 
+	/**
+	 * @brief Load all plugins from search paths
+	 */
 	void loadPlugins();
 
+	/**
+	 * @brief Get loaded plugins
+	 * @return List of plugins
+	 */
 	ccPluginInterfaceList& pluginList();
 
+	/**
+	 * @brief Enable or disable a plugin
+	 * @param[in] plugin Plugin to modify
+	 * @param[in] enabled Enable state
+	 */
 	void setPluginEnabled(const ccPluginInterface* plugin, bool enabled);
+	
+	/**
+	 * @brief Check if plugin is enabled
+	 * @param[in] plugin Plugin to check
+	 * @return true if enabled
+	 */
 	bool isEnabled(const ccPluginInterface* plugin) const;
 
   protected:
+	/**
+	 * @brief Create plugin manager
+	 * @param[in] parent Parent object
+	 */
 	explicit ccPluginManager(QObject* parent = nullptr);
 
   private: // methods
+	/// Load plugins from paths
 	void loadFromPathsAndAddToList();
 
+	/// Get list of disabled plugin IIDs
 	void getDisabledPluginIIDs(QStringList& disabledPlugins) const;
 
   private: // members
-	QStringList           m_pluginPaths;
+	/// Plugin search paths
+	QStringList m_pluginPaths;
+	/// Loaded plugins
 	ccPluginInterfaceList m_pluginList;
 };
