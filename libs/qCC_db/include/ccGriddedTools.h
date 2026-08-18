98991599dbfa160ea50a09b4ecfa70bedf458e08
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:38:39 +0100

    docs: Add Doxygen comments to ccGriddedTools.h
    - Add file-level documentation
    - Document ccGriddedTools class
    - Document GridParameters struct
    - Document DetectParameters, ComputeBestSensor methods
    
    Tools for gridded point clouds.

--- libs/qCC_db/include/ccGriddedTools.h
+++ libs/qCC_db/include/ccGriddedTools.h
@@ -18,19 +18,36 @@
 #ifndef CC_GRIDDED_CLOUD_TOOLS_HEADER
 #define CC_GRIDDED_CLOUD_TOOLS_HEADER
 
+/**
+ * @file ccGriddedTools.h
+ *
+ * @brief Gridded cloud tools
+ *
+ * Tools for working with gridded point clouds.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
+
 // Local
 #include "ccPointCloud.h"
 
 class ccGBLSensor;
 class ccGLMatrix;
 
-//! Tools dedicated to gridded clouds
+/**
+ * @brief Gridded cloud tools
+ *
+ * Utilities for gridded point clouds.
+ */
 class QCC_DB_LIB_API ccGriddedTools
 {
   public:
-	//! Grid (angular) parameters
+	/**
+	 * @brief Grid parameters
+	 */
 	struct GridParameters
 	{
+		/// Default constructor
 		GridParameters()
 		    : minPhi(0)
 		    , maxPhi(0)
@@ -48,26 +65,28 @@ class QCC_DB_LIB_API ccGriddedTools
 		PointCoordinateType maxRange;
 	};
 
-	//! Detects the given grid parameters (angular span, etc.)
-	/** \param cloud associated cloud
-	    \param grid scan grid
-	    \param parameters output parameters
-	    \param verbose whether the process should output some detailed information in the log/console or not
-	    \param cloudToSensorTrans transformation from cloud coordinate system to the sensor coordinate system (optional)
-	    \return success
-	**/
+	/**
+	 * @brief Detect grid parameters
+	 * @param[in] cloud Associated cloud
+	 * @param[in] grid Scan grid
+	 * @param[out] parameters Output parameters
+	 * @param[in] verbose Verbose output
+	 * @param[in] cloudToSensorTrans Optional transform
+	 * @return true on success
+	 */
 	static bool DetectParameters(const ccPointCloud*              cloud,
 	                             const ccPointCloud::Grid::Shared grid,
 	                             GridParameters&                  parameters,
 	                             bool                             verbose            = false,
 	                             ccGLMatrix*                      cloudToSensorTrans = nullptr);
 
-	//! Determines the (TLS) sensor parameters from the relative position of gridded points
-	/** \param cloud cloud on which to compute the sensor parameters (should be a single grid)
-	    \param grid scan grid
-	    \param cloudToSensorTrans transformation from cloud coordinate system to the sensor coordinate system (optional)
-	    \return sensor (if successful) or 0 otherwise
-	**/
+	/**
+	 * @brief Compute best sensor from grid
+	 * @param[in] cloud Point cloud
+	 * @param[in] grid Scan grid
+	 * @param[in] cloudToSensorTrans Optional transform
+	 * @return Sensor, or nullptr on failure
+	 */
 	static ccGBLSensor* ComputeBestSensor(ccPointCloud*              cloud,
 	                                      ccPointCloud::Grid::Shared grid,
 	                                      ccGLMatrix*                cloudToSensorTrans = nullptr);
