f875390c4328b66513190e878b426f28f4787940
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:53:11 +0100

    docs: Add Doxygen comments to ccCommandCrossSection.h
    - Add file-level documentation
    - Document CommandCrossSection struct
    
    Cross section command.

--- qCC/ccCommandCrossSection.h
+++ qCC/ccCommandCrossSection.h
@@ -1,15 +1,32 @@
 #ifndef COMMAND_CROSS_SECTION_HEADER
 #define COMMAND_CROSS_SECTION_HEADER
 
+/**
+ * @file ccCommandCrossSection.h
+ *
+ * @brief Cross section command
+ *
+ * Command-line cross section command.
+ *
+ * @author CloudCompare project
+ */
+
 #include "ccCommandLineInterface.h"
 
 class QString;
 class QXmlStreamAttributes;
 
+/**
+ * @brief Cross section command
+ *
+ * Process cross section from command line.
+ */
 struct CommandCrossSection : public ccCommandLineInterface::Command
 {
+	/// Constructor
 	CommandCrossSection();
 
+	/// Process command
 	bool process(ccCommandLineInterface& cmd) override;
 
   private:
