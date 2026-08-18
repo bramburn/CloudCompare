a18bcb780085bb1fc4cc5228ecef155ea8702245
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 22:40:44 +0100

    docs: Add Doxygen comments to ccPinchNode.h
    - Add file and class documentation
    - Document ccPinchNode class
    
    Pinch node.

--- plugins/core/Standard/qCompass/include/ccPinchNode.h
+++ plugins/core/Standard/qCompass/include/ccPinchNode.h
@@ -18,24 +18,48 @@
 #ifndef CC_NODE_HEADER
 #define CC_NODE_HEADER
 
+/**
+ * @file ccPinchNode.h
+ *
+ * @brief Pinch node class
+ *
+ * Represents pinch nodes created with qCompass.
+ */
+
 #include "ccPointPair.h"
 
 #include <ccPointCloud.h>
 
-/*
-Simple class used to create/represent/draw pinch-nodes created using qCompass
-*/
+/**
+ * @class ccPinchNode
+ *
+ * @brief Pinch node
+ *
+ * Represents/draws pinch nodes created with qCompass.
+ */
 class ccPinchNode : public ccPointPair
 {
 public:
-	//ctors
+	/**
+	 * @brief Create pinch node
+	 * @param[in] associatedCloud Associated point cloud
+	 */
 	ccPinchNode(ccPointCloud* associatedCloud);
+	
+	/**
+	 * @brief Create from polyline
+	 * @param[in] obj Source polyline
+	 */
 	ccPinchNode(ccPolyline* obj);
 
-	//write metadata specific to this object
+	/// Update metadata
 	void updateMetadata() override;
 
-	//returns true if obj is/was a pinchNode (as recorded by its metadata)
+	/**
+	 * @brief Check if object is a pinch node
+	 * @param[in] obj Object to check
+	 * @return True if pinch node
+	 */
 	static bool isPinchNode(ccHObject* obj);
 };
 
