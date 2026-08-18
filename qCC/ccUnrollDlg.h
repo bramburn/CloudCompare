469a3343096ede2e47fdfd521c031c199ea38dc8
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:11:45 +0100

    docs: Add Doxygen comments to ccUnrollDlg.h
    - Add file-level documentation
    - Document ccUnrollDlg class
    
    Unroll dialog.

--- qCC/ccUnrollDlg.h
+++ qCC/ccUnrollDlg.h
@@ -17,6 +17,16 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file ccUnrollDlg.h
+ *
+ * @brief Unroll dialog
+ *
+ * Dialog for unrolling clouds on cylinder or cone.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
+
 #include <QDialog>
 
 // qCC_db
@@ -27,39 +37,70 @@ namespace Ui
 	class UnrollDialog;
 }
 
-//! Dialog: unroll a cloud on a cylinder or a cone
+/**
+ * @brief Unroll dialog
+ *
+ * Unroll clouds on cylinder or cone.
+ */
 class ccUnrollDlg : public QDialog
 {
 	Q_OBJECT
 
   public:
-	//! Default constructor
+	/**
+	 * @brief Create dialog
+	 * @param[in] dbRootEntity Database root entity
+	 * @param[in] parent Parent widget
+	 */
 	explicit ccUnrollDlg(ccHObject* dbRootEntity, QWidget* parent = nullptr);
+	
+	/// Destructor
 	~ccUnrollDlg() override;
 
+	/// Get unroll type
 	ccPointCloud::UnrollMode getType() const;
-	CCVector3d               getAxis() const;
-	bool                     isAxisPositionAuto() const;
-	bool                     useArbitraryOutputCS() const;
-	bool                     removeStretchedTriangles() const;
-	CCVector3                getAxisPosition() const;
-	void                     getAngleRange(double& start_deg, double& stop_deg) const;
-	double                   getRadius() const;
-	double                   getConeHalfAngle() const;
-	bool                     exportDeviationSF() const;
-	double                   getConicalProjSpanRatio() const;
+	/// Get axis
+	CCVector3d getAxis() const;
+	/// Get axis position auto flag
+	bool isAxisPositionAuto() const;
+	/// Get use arbitrary output CS flag
+	bool useArbitraryOutputCS() const;
+	/// Get remove stretched triangles flag
+	bool removeStretchedTriangles() const;
+	/// Get axis position
+	CCVector3 getAxisPosition() const;
+	/// Get angle range
+	void getAngleRange(double& start_deg, double& stop_deg) const;
+	/// Get radius
+	double getRadius() const;
+	/// Get cone half angle
+	double getConeHalfAngle() const;
+	/// Get export deviation SF flag
+	bool exportDeviationSF() const;
+	/// Get conical proj span ratio
+	double getConicalProjSpanRatio() const;
 
+	/// Save to persistent settings
 	void toPersistentSettings() const;
+	/// Load from persistent settings
 	void fromPersistentSettings();
+	/// Set configuration
 	void setConfiguration(bool cloudsOnly);
 
   protected:
+	/// Handle shape type changed
 	void shapeTypeChanged(int index);
+	/// Handle projection type changed
 	void projectionTypeChanged(int index);
+	/// Handle axis dimension changed
 	void axisDimensionChanged(int index);
+	/// Handle axis auto state changed
 	void axisAutoStateChanged(int checkState);
+	/// Load parameters from entity
 	void loadParametersFromEntity();
+	/// Set axis from clipboard
 	void axisFromClipboard();
+	/// Set center from clipboard
 	void centerFromClipboard();
 
   protected:
