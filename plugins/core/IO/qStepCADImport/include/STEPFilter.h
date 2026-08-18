dd78c7b222e6a27a0e7299111843b0170b8c927d
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 22:01:41 +0100

    docs: Add Doxygen comments to STEPFilter.h
    - Add file-level documentation
    - Document STEPFilter class
    
    STEP file filter.

--- plugins/core/IO/qStepCADImport/include/STEPFilter.h
+++ plugins/core/IO/qStepCADImport/include/STEPFilter.h
@@ -6,39 +6,64 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                          COPYRIGHT: EDF R&D                            #
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file STEPFilter.h
+ *
+ * @brief STEP file filter
+ *
+ * I/O filter for STEP CAD files.
+ *
+ * @author EDF R&D
+ */
+
 #include <FileIOFilter.h>
 
+/**
+ * @class STEPFilter
+ *
+ * @brief STEP file filter
+ *
+ * Import STEP CAD files.
+ */
 class STEPFilter : public FileIOFilter
 {
   public:
+	/// Constructor
 	STEPFilter();
 
-	// inherited from FileIOFilter
+	/// Load file
 	CC_FILE_ERROR loadFile(const QString& fullFilename, ccHObject& container, LoadParameters& parameters) override;
 
-	//! Specific loading method
+	/**
+	 * @brief Import STEP file
+	 * @param[out] container Container for loaded entities
+	 * @param[in] fullFilename Full path to file
+	 * @param[in] linearDeflection Triangle deflection value
+	 * @param[in] parameters Load parameters
+	 * @return Error code
+	 */
 	CC_FILE_ERROR importStepFile(ccHObject&      container,
 	                             const QString&  fullFilename,
 	                             double          linearDeflection,
 	                             LoadParameters& parameters);
 
-	//! Sets the default linear deflection
-	/** \param value linear deflection (in [1e-2, 1e-6])
-	        The smaller this value is, the smaller the triangles will be.
-	        In some cases, if this linear deflection is too big, the tesselation may crash
-	        (precisely the instruction BRep_Tool::Triangulation(face, location)).
-	        But we don't know how to anticipate this.
-	**/
+	/**
+	 * @brief Set default linear deflection
+	 * @param[in] value Linear deflection (in [1e-2, 1e-6])
+	 *
+	 * Smaller value = smaller triangles.
+	 * If too large, tessellation may crash.
+	 */
 	static void SetDefaultLinearDeflection(double value);
 };
