1c4fc71d3ea7b032cb4ca66210873c49edd8930b
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:37:29 +0100

    docs: Add Doxygen comments to qDracoIO.h
    - Add file-level documentation
    - Document qDracoIO plugin class
    
    Draco I/O plugin.

--- plugins/core/IO/qDracoIO/include/qDracoIO.h
+++ plugins/core/IO/qDracoIO/include/qDracoIO.h
@@ -17,8 +17,23 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file qDracoIO.h
+ *
+ * @brief Draco I/O plugin
+ *
+ * Plugin for Google Draco compressed mesh I/O.
+ *
+ * @author CloudCompare project
+ */
+
 #include <ccIOPluginInterface.h>
 
+/**
+ * @brief Draco I/O plugin
+ *
+ * Read/write Google Draco compressed meshes.
+ */
 class qDracoIO : public QObject
     , public ccIOPluginInterface
 {
@@ -28,9 +43,15 @@ class qDracoIO : public QObject
 	Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.qDracoIO" FILE "../info.json")
 
   public:
+	/**
+	 * @brief Create plugin
+	 * @param[in] parent Parent object
+	 */
 	explicit qDracoIO(QObject* parent = nullptr);
 
+	/// Register command line commands
 	void registerCommands(ccCommandLineInterface* cmd) override;
 
+	/// Get list of supported filters
 	FilterList getFilters() override;
 };
