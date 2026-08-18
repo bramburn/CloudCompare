a85f2b055330d71fedfd66c7e4adfb2c270682e9
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:15:51 +0100

    docs: Add Doxygen comments to ccShortcutDialog.h
    - Add file-level documentation
    - Document ccShortcutDialog and ccShortcutEditDialog classes
    
    Shortcut dialogs.

--- qCC/ccShortcutDialog.h
+++ qCC/ccShortcutDialog.h
@@ -1,43 +1,70 @@
 #ifndef CC_SHORTCUTDIALOG_H
 #define CC_SHORTCUTDIALOG_H
 
+/**
+ * @file ccShortcutDialog.h
+ *
+ * @brief Shortcut dialog
+ *
+ * Dialogs for editing keyboard shortcuts.
+ */
+
 #include "ui_shorcutSettings.h"
 #include "ui_shortcutEditDialog.h"
 
-//! Widget that captures key sequences to be able to edit a shortcut assigned to
-//! an action
+/**
+ * @brief Shortcut edit dialog
+ *
+ * Capture key sequences for editing shortcuts.
+ */
 class ccShortcutEditDialog final : public QDialog
 {
 	Q_OBJECT
 
   public:
+	/**
+	 * @brief Create dialog
+	 * @param[in] parent Parent widget
+	 */
 	explicit ccShortcutEditDialog(QWidget* parent = nullptr);
 
+	/// Get key sequence
 	QKeySequence keySequence() const;
 
+	/// Set key sequence
 	void setKeySequence(const QKeySequence& sequence) const;
 
+	/// Execute dialog
 	int exec() override;
 
   private:
 	Ui_ShortcutEditDialog* m_ui;
 };
 
-//! Shortcut edit dialog
-//!
-//! List shortcuts for known actions, and allows to edit them
-//! Saves to QSettings on each edit
+/**
+ * @brief Shortcut dialog
+ *
+ * List and edit shortcuts for known actions.
+ */
 class ccShortcutDialog final : public QDialog
 {
 	Q_OBJECT
   public:
+	/**
+	 * @brief Create dialog
+	 * @param[in] actions Available actions
+	 * @param[in] parent Parent widget
+	 */
 	explicit ccShortcutDialog(const QList<QAction*>& actions, QWidget* parent = nullptr);
 
+	/// Restore shortcuts from settings
 	void restoreShortcutsFromQSettings() const;
 
   private:
+	/// Check for conflict
 	const QAction* checkConflict(const QKeySequence& sequence) const;
-	void           handleDoubleClick(QTableWidgetItem* item);
+	/// Handle double click
+	void handleDoubleClick(QTableWidgetItem* item);
 
 	Ui_ShortcutDialog*    m_ui;
 	ccShortcutEditDialog* m_editDialog;
