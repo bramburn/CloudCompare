fdfce81b650be5167043a175f0cf6a9b7df2bba1
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:49:21 +0100

    docs: Add Doxygen comments to ccBoundingBoxEditorDlg.h
    - Add file-level documentation
    - Document ccBoundingBoxEditorDlg class
    
    Bounding box editor dialog.

--- qCC/ccBoundingBoxEditorDlg.h
+++ qCC/ccBoundingBoxEditorDlg.h
@@ -18,63 +18,88 @@
 #ifndef CC_BOUNDING_BOX_EDITOR_DLG_HEADER
 #define CC_BOUNDING_BOX_EDITOR_DLG_HEADER
 
+/**
+ * @file ccBoundingBoxEditorDlg.h
+ *
+ * @brief Bounding box editor dialog
+ *
+ * Dialog for editing 3D bounding box extents.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
+
 #include <ui_boundingBoxEditorDlg.h>
 
 // qCC_db
 #include <ccBBox.h>
 
-//! Dialog to define the extents of a 3D box
+/**
+ * @brief Bounding box editor dialog
+ *
+ * Dialog for editing 3D bounding box dimensions.
+ */
 class ccBoundingBoxEditorDlg : public QDialog
     , public Ui::BoundingBoxEditorDialog
 {
 	Q_OBJECT
 
   public:
-	//! Default constructor
-	/** \param showBoxAxes			Whether to display the box axes or not
-	    \param showRasterGridImage	Whether to display the raster grid image or not
-	    \param parent				Parent widget
-	**/
+	/**
+	 * @brief Create dialog
+	 * @param[in] showBoxAxes Show box axes
+	 * @param[in] showRasterGridImage Show raster grid image
+	 * @param[in] parent Parent widget
+	 */
 	explicit ccBoundingBoxEditorDlg(bool showBoxAxes, bool showRasterGridImage, QWidget* parent = nullptr);
 
-	//! Returns bounding box
+	/**
+	 * @brief Get bounding box
+	 * @return Current bounding box
+	 */
 	inline const ccBBox& getBox() const
 	{
 		return m_currentBBox;
 	}
 
-	//! Overrides the current bounding box
-	/** \param box bounding box
-	 **/
+	/**
+	 * @brief Set bounding box
+	 * @param[in] box Bounding box
+	 */
 	void setBox(const ccBBox& box);
 
-	//! Sets the (minimal) base box
-	/** \param box base box
-	    \param isMinimal set whether the user must define a bounding-box at least as large as this one
-	**/
+	/**
+	 * @brief Set base box
+	 * @param[in] box Base box
+	 * @param[in] isMinimal Require box to be at least this large
+	 */
 	void setBaseBBox(const ccBBox& box, bool isMinimal = true);
 
-	//! Sets the box axes
+	/// Set box axes
 	void setBoxAxes(const CCVector3& X, const CCVector3& Y, const CCVector3& Z);
 
-	//! Returns the box axes
+	/// Get box axes
 	void getBoxAxes(CCVector3d& X, CCVector3d& Y, CCVector3d& Z);
 
-	//! Whether the warning about bounding box inclusion in the base one should be displayed or not
-	/** True by default.
-	 **/
+	/**
+	 * @brief Show inclusion warning
+	 * @param[in] state Show state
+	 */
 	void showInclusionWarning(bool state)
 	{
 		m_showInclusionWarning = state;
 	}
 
-	//! Forces the 'keep square' mode
+	/// Force keep square mode
 	void forceKeepSquare(bool state);
 
-	//! Returns whether 'keep square' mode is enabled or not
+	/// Check keep square mode
 	bool keepSquare() const;
 
-	//! Sets 2D mode (the line 'dim' will be hidden)
+	/**
+	 * @brief Set 2D mode
+	 * @param[in] state 2D mode
+	 * @param[in] dim Hidden dimension
+	 */
 	void set2DMode(bool state, unsigned char dim);
 
   public:
@@ -93,34 +118,21 @@ class ccBoundingBoxEditorDlg : public QDialog
 	void updateYWidth(double);
 	void updateZWidth(double);
 
-	//! Updates current box based on the dialog state
 	void updateCurrentBBox(double dummy = 0.0);
-	//! Reflects changes on bbox
 	void reflectChanges(int dummy = 0);
 
-	//! Slot called anytime a component of the box axes is modified
 	void onAxisValueChanged(double);
 
 	void fromClipboardClicked();
 	void toClipboardClicked();
 
   protected:
-	//! Checks if currentBox includes baseBox
 	void checkBaseInclusion();
 
-	//! Base box (invalid if none)
 	ccBBox m_baseBBox;
-
-	//! Whether base box is minimal or not
 	bool m_baseBoxIsMinimal;
-
-	//! Current box
 	ccBBox m_currentBBox;
-
-	//! Box state at dialog start
 	ccBBox m_initBBox;
-
-	//! Whether to show 'inclusion' warning or not
 	bool m_showInclusionWarning;
 };
 
