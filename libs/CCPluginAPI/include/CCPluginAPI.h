d42af5c781a596f62b418a0e5823434584cfec2d
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:34:19 +0100

    docs: Add Doxygen comments to CCPluginAPI.h
    - Add file-level documentation
    
    Plugin API library export definitions.

--- libs/CCPluginAPI/include/CCPluginAPI.h
+++ libs/CCPluginAPI/include/CCPluginAPI.h
@@ -6,16 +6,24 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 or later of the License.      #
+// #  the Free Software Foundation; version 2 of the License.              #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #          COPYRIGHT: CloudCompare project                               #
 // #                                                                        #
 // ##########################################################################
+
+/**
+ * @file CCPluginAPI.h
+ *
+ * @brief Plugin API library export definitions
+ *
+ * Defines the CCPLUGIN_LIB_API macro for library export/import.
+ */
 
 #include <QtCore/QtGlobal>
 
