a97f5bd0f629e5c39bebb627358dfeaa2ead2d68
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 22:26:42 +0100

    docs: Add Doxygen comments to ccColorStyledDelegate.h
    - Add file and class documentation
    - Document ccColorStyledDelegate class
    
    Color delegate.

--- plugins/core/Standard/qCloudLayers/include/ccColorStyledDelegate.h
+++ plugins/core/Standard/qCloudLayers/include/ccColorStyledDelegate.h
@@ -6,7 +6,7 @@
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
-//#  the Free Software Foundation; version 2 of the License.               #
+//#  the Free Software Foundation; version 2 or the License.               #
 //#                                                                        #
 //#  This program is distributed in the hope that it will be useful,       #
 //#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -17,17 +17,36 @@
 //#                                                                        #
 //##########################################################################
 
-//QT
+/**
+ * @file ccColorStyledDelegate.h
+ *
+ * @brief Color styled delegate
+ *
+ * Custom item delegate for color display in table.
+ */
+
 #include <QStyledItemDelegate>
 
 class QPainter;
 
+/**
+ * @class ccColorStyledDelegate
+ *
+ * @brief Color styled delegate
+ *
+ * Custom delegate for rendering color cells.
+ */
 class ccColorStyledDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
 
 public:
+	/**
+	 * @brief Create delegate
+	 * @param[in] parent Parent object
+	 */
 	ccColorStyledDelegate(QObject* parent = nullptr);
+	
+	/// Paint color cell
 	void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
 };
-
