7a5f80adb7b5e444d32553f02e62868eea27dd0f
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:51:26 +0100

    docs: Add Doxygen comments to LasWaveformLoader.h
    - Add file and struct documentation
    - Document LasWaveformLoader struct
    
    LAS waveform loader.

--- plugins/core/IO/qLASIO/include/LasWaveformLoader.h
+++ plugins/core/IO/qLASIO/include/LasWaveformLoader.h
@@ -6,7 +6,7 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -17,6 +17,16 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file LasWaveformLoader.h
+ *
+ * @brief LAS waveform loader
+ *
+ * Load waveform data from LAS files.
+ *
+ * @author Thomas Montaigu
+ */
+
 // Qt
 #include <QFileInfo>
 #include <QString>
@@ -25,12 +35,30 @@
 // LASzip
 #include <laszip/laszip_api.h>
 
+/**
+ * @struct LasWaveformLoader
+ *
+ * @brief LAS waveform loader
+ *
+ * Load waveform data from LAS files.
+ */
 struct LasWaveformLoader
 {
+	/**
+	 * @brief Create waveform loader
+	 * @param[in] laszipHeader LAS header
+	 * @param[in] lasFilename LAS filename
+	 * @param[in] pointCloud Point cloud
+	 */
 	LasWaveformLoader(const laszip_header_struct& laszipHeader,
 	                  const QString&              lasFilename,
 	                  ccPointCloud&               pointCloud);
 
+	/**
+	 * @brief Load waveform for point
+	 * @param[in,out] pointCloud Point cloud
+	 * @param[in] currentPoint Current point
+	 */
 	void loadWaveform(ccPointCloud& pointCloud, const laszip_point& currentPoint) const;
 
 	uint64_t                       fwfDataCount{0};
