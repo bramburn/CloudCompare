3d2a9dead3584325a6f1b3013d03d36dcf2eca97
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 20:14:16 +0100

    docs: Add Doxygen comments to ccColorScalesManager.h
    - Add file-level documentation
    - Document DEFAULT_SCALES enum with all color presets
    - Document singleton access methods
    
    Color scales manager with 19 predefined color ramps.

--- libs/qCC_db/include/ccColorScalesManager.h
+++ libs/qCC_db/include/ccColorScalesManager.h
@@ -18,47 +18,74 @@
 #ifndef CC_COLOR_SCALES_MANAGER_HEADER
 #define CC_COLOR_SCALES_MANAGER_HEADER
 
+/**
+ * @file ccColorScalesManager.h
+ *
+ * @brief Color scales manager singleton
+ *
+ * Manages color scale presets for scalar field visualization.
+ * Provides access to pre-defined color ramps like BGYR, grey,
+ * HSV, Viridis, and ASPRS classification colors.
+ *
+ * @author EDF R&D / TELECOM ParisTech (ENST-TSI)
+ */
 // Local
 #include "ccColorScale.h"
 
 // Qt
 #include <QMap>
 
-//! Color scales manager/container
+/**
+ * @brief Color scales manager singleton
+ *
+ * Manages and provides access to pre-defined color scales.
+ */
 class QCC_DB_LIB_API ccColorScalesManager
 {
   public:
-	//! Returns unique instance
+	/**
+	 * @brief Get singleton instance
+	 * @return Pointer to the manager
+	 */
 	static ccColorScalesManager* GetUniqueInstance();
 
-	//! Releases unique instance
+	/**
+	 * @brief Release singleton instance
+	 */
 	static void ReleaseUniqueInstance();
 
-	//! Destructor
+	/**
+	 * @brief Destructor
+	 */
 	virtual ~ccColorScalesManager();
 
-	//! Pre-defined color scales (all relative - i.e. expand to actual SF)
+	/**
+	 * @brief Pre-defined color scales
+	 *
+	 * All scales use relative values (0-1) that expand
+	 * to the actual scalar field range.
+	 */
 	enum DEFAULT_SCALES
 	{
-		BGYR              = 0,  /**< Blue-Green-Yellow-Red ramp (default for distances display) */
-		GREY              = 1,  /**< Grey ramp (default for Global Illumination) */
-		BWR               = 2,  /**< Blue-White-Red ramp (for signed SF)*/
-		RY                = 3,  /**< Red-Yellow ramp */
-		RW                = 4,  /**< Red-White ramp */
-		ABS_NORM_GREY     = 5,  /**< Absolute normalized grey ramp (intensities between 0 and 1) */
-		HSV_360_DEG       = 6,  /**< HSV colors between 0 and 360 degrees */
-		VERTEX_QUALITY    = 7,  /**< Mesh vertex quality (see CCCoreLib::MeshSamplingTools::VertexFlags) */
-		DIP_BRYW          = 8,  /**< Dip (0 - 90 degrees) (Brown-Red-Yellow-White) */
-		DIP_DIR_REPEAT    = 9,  /**< Dip direction (0 - 360 degrees) */
-		VIRIDIS           = 10, /**< matplotlib library colorscale created by Stéfan van der Walt and Nathaniel Smith */
-		BROWN_YELLOW      = 11, /**< Brown-Yellow */
-		YELLOW_BROWN      = 12, /**< Yellow-Brown */
-		TOPO_LANDSERF     = 13, /**< Topo Landserf (quartile) */
-		HIGH_CONTRAST     = 14, /**< High contrast */
-		CIVIDIS           = 15, /**< matplotlib library colorscale - see https://arxiv.org/ftp/arxiv/papers/1712/1712.01662.pdf */
-		ASPRS_CLASSES     = 16, /**< ASPRS classes */
-		ASPRS_WITH_LABELS = 17, /**< ASPRS classes with labels */
-		GREY_INV          = 18, /**< Inverted grey ramp */
+		BGYR              = 0,  //!< Blue-Green-Yellow-Red (distances)
+		GREY              = 1,  //!< Grey ramp
+		BWR               = 2,  //!< Blue-White-Red (signed SF)
+		RY                = 3,  //!< Red-Yellow
+		RW                = 4,  //!< Red-White
+		ABS_NORM_GREY     = 5,  //!< Absolute normalized grey
+		HSV_360_DEG       = 6,  //!< HSV 0-360 degrees
+		VERTEX_QUALITY    = 7,  //!< Mesh vertex quality
+		DIP_BRYW          = 8,  //!< Dip 0-90 degrees
+		DIP_DIR_REPEAT    = 9,  //!< Dip direction 0-360 degrees
+		VIRIDIS           = 10, //!< Matplotlib Viridis
+		BROWN_YELLOW      = 11, //!< Brown-Yellow
+		YELLOW_BROWN      = 12, //!< Yellow-Brown
+		TOPO_LANDSERF     = 13, //!< Topo Landserf
+		HIGH_CONTRAST     = 14, //!< High contrast
+		CIVIDIS           = 15, //!< Matplotlib Cividis
+		ASPRS_CLASSES     = 16, //!< ASPRS classes
+		ASPRS_WITH_LABELS = 17, //!< ASPRS with labels
+		GREY_INV          = 18, //!< Inverted grey
 	};
 
 	//! Returns a pre-defined color scale UUID
