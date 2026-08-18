2769769abb3dff957be60b9a2c3349b301a846d6
Kaps Ramburn <nitrogen@gmail.com>
2026-08-18 21:46:23 +0100

    docs: Add Doxygen comments to LasPlugin.h
    - Add file-level documentation
    - Document LasPlugin class
    
    LAS I/O plugin.

--- plugins/core/IO/qLASIO/include/LasPlugin.h
+++ plugins/core/IO/qLASIO/include/LasPlugin.h
@@ -6,19 +6,34 @@
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
 // #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+// #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file LasPlugin.h
+ *
+ * @brief LAS I/O plugin
+ *
+ * Plugin for LAS/LAZ LiDAR file I/O.
+ *
+ * @author Thomas Montaigu
+ */
+
 #include "ccIOPluginInterface.h"
 
+/**
+ * @brief LAS I/O plugin
+ *
+ * Read/write LAS/LAZ LiDAR point cloud files.
+ */
 class LasPlugin : public QObject
     , public ccIOPluginInterface
 {
@@ -28,9 +43,15 @@ class LasPlugin : public QObject
 	Q_PLUGIN_METADATA(IID "cccorp.cloudcompare.plugin.LASIO" FILE "../info.json")
 
   public:
+	/**
+	 * @brief Create plugin
+	 * @param[in] parent Parent object
+	 */
 	explicit LasPlugin(QObject* parent = nullptr);
+	
+	/// Destructor
 	~LasPlugin() override = default;
 
-	// Inherited from ccIOPluginInterface
+	/// Get list of supported filters
 	ccIOPluginInterface::FilterList getFilters() override;
 };
