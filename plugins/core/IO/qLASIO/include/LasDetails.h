1b326f66a5b754efa1a0aae782cdf76a5d85d1c8
Mavis <Mavis@mavis.local>
2026-08-20 13:47:53 +0100

    docs: enhance LasDetails.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasDetails.h
+++ plugins/core/IO/qLASIO/include/LasDetails.h
@@ -1,3 +1,21 @@
+/**
+ * @file LasDetails.h
+ *
+ * @brief LAS format details and utilities.
+ *
+ * @details Core definitions for LAS/LAZ file handling.
+ *
+ * ## Features
+ *
+ * - LAS version and point format constants
+ * - Point format size calculations
+ * - Extended VLR header structures
+ * - Chunk interval management (COPC)
+ * - Version selection helpers
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,7 +23,6 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 of the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
@@ -14,7 +31,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 // CCCorelib
@@ -39,145 +56,217 @@ struct laszip_header;
 struct laszip_vlr;
 typedef laszip_vlr laszip_vlr_struct;
 
-constexpr size_t LAS_VLR_HEADER_SIZE = 54;
-constexpr double SCAN_ANGLE_SCALE    = 0.006;
+constexpr size_t LAS_VLR_HEADER_SIZE = 54; //!< VLR header size.
+constexpr double SCAN_ANGLE_SCALE    = 0.006; //!< Scan angle scale factor.
 
 /**
- * @namespace LasNames
- *
- * @brief Scalar field names for LAS standard dimensions
+ * @brief LAS standard dimension names.
  *
  * Standard dimension names as defined by the LAS specification.
  */
 namespace LasNames
 {
+	//! Intensity field name.
 	constexpr const char* Intensity         = "Intensity";
+	//! Return number field name.
 	constexpr const char* ReturnNumber      = "Return Number";
+	//! Number of returns field name.
 	constexpr const char* NumberOfReturns   = "Number Of Returns";
+	//! Scan direction flag field name.
 	constexpr const char* ScanDirectionFlag = "Scan Direction Flag";
+	//! Edge of flight line field name.
 	constexpr const char* EdgeOfFlightLine  = "EdgeOfFlightLine";
+	//! Classification field name.
 	constexpr const char* Classification    = "Classification";
+	//! Synthetic flag field name.
 	constexpr const char* SyntheticFlag     = "Synthetic Flag";
+	//! Keypoint flag field name.
 	constexpr const char* KeypointFlag      = "Keypoint Flag";
+	//! Withheld flag field name.
 	constexpr const char* WithheldFlag      = "Withheld Flag";
+	//! Scan angle rank field name.
 	constexpr const char* ScanAngleRank     = "Scan Angle Rank";
+	//! User data field name.
 	constexpr const char* UserData          = "User Data";
+	//! Point source ID field name.
 	constexpr const char* PointSourceId     = "Point Source ID";
+	//! GPS time field name.
 	constexpr const char* GpsTime           = "Gps Time";
 
 	// 1.4 point format 6 stuff
+	//! Scan angle field name.
 	constexpr const char* ScanAngle      = "Scan Angle";
+	//! Scanner channel field name.
 	constexpr const char* ScannerChannel = "Scanner Channel";
+	//! Overlap flag field name.
 	constexpr const char* OverlapFlag    = "Overlap Flag";
+	//! Near infrared field name.
 	constexpr const char* NearInfrared   = "Near Infrared";
 } // namespace LasNames
 
+/**
+ * @brief LAS format details namespace.
+ */
 namespace LasDetails
 {
-	/// Unscaled Extent for LAS or COPC
-	///
-	/// This is an alias for a double-precision bounding box (BB) that is meant to represent:
-	/// - The unscaled extent of a LAS file (as defined in the header), or
-	/// - The unscaled bounding box of a COPC file (derived from copc::Info -> center/halfsize variables).
-	///
-	/// Note: The COPC extent is cubic, corresponding to the COPC octree shape,
-	/// rather than the bounding box of the actual points.
+	/// @brief Unscaled extent type.
+	/// @details Alias for double-precision bounding box:
+	/// - LAS file extent (from header)
+	/// - COPC file extent (from copc::Info)
+	/// @note COPC extent is cubic (octree shape)
 	using UnscaledExtent = CCCoreLib::BoundingBoxTpl<double>;
 
-	/// LAZ Chunk but with point offset in file.
-	///
-	/// This is primarily used for COPC reading.
-	/// Since the LASZip API does not allow chunk byte seeking, we use the point offset for seeking.
-	/// This data structure can also encode the offset in the current CC point cloud,
-	/// which is useful for the LOD data structure construction.
-	/// Additionally, it includes a status field that indicates whether it should be loaded (PASS),
-	/// checked during loading (INTERSECT_BB), or pruned (FAIL).
+	/**
+	 * @brief LAZ chunk interval with file offset.
+	 *
+	 * Used for COPC reading. Includes point offset for seeking
+	 * since LASzip API doesn't support byte seeking.
+	 */
 	struct ChunkInterval
 	{
+		/**
+		 * @brief Filter status enumeration.
+		 */
 		enum class eFilterStatus
 		{
-			PASS         = 0,
-			INTERSECT_BB = 1,
-			FAIL         = 2,
+			PASS         = 0, //!< Load chunk.
+			INTERSECT_BB = 1, //!< Check intersection.
+			FAIL         = 2  //!< Skip chunk.
 		};
 
+		//! Default constructor.
 		ChunkInterval() = default;
+
+		/**
+		 * @brief Create chunk interval.
+		 *
+		 * @param[in] _pointOffsetInFile Offset in file.
+		 * @param[in] _pointCount Number of points.
+		 */
 		ChunkInterval(uint64_t _pointOffsetInFile, uint64_t _pointCount)
 		    : pointOffsetInFile(_pointOffsetInFile)
-		    , pointCount(_pointCount) {};
+		    , pointCount(_pointCount) {}
 
-		/// point offset in the LAZ file
+		//! Offset in LAS file.
 		uint64_t pointOffsetInFile{0};
-		/// point offset in the CC cloud (used for LOD construction)
+
+		//! Offset in CC cloud (LOD construction).
 		uint64_t pointOffsetInCCCloud{0};
-		/// point count in the chunk
+
+		//! Point count in chunk.
 		uint64_t pointCount{0};
-		/// Number of points that failed the filter pass (optional)
-		/// total point for this chunk in the ccPointCloud
-		/// should be pointCount - filteredPointCount
+
+		//! Filtered point count.
 		uint64_t filteredPointCount{0};
-		/// encode the status of the chunk
-		/// basically chunks with FAIL status should not be loaded.
+
+		//! Filter status.
 		eFilterStatus status{eFilterStatus::PASS};
 	};
 
-	/// the "true" number of points in a las file.
-	/// the field to query in the laszip_header in order to have the number of point
-	/// is format dependant.
+	/**
+	 * @brief Get true point count from header.
+	 *
+	 * @param[in] laszipHeader LASzip header.
+	 *
+	 * @return True number of points.
+	 */
 	uint64_t TrueNumberOfPoints(const laszip_header* laszipHeader);
 
-	// The position of the overlap flag in the classification flags
-	// (valid for fmt >= 6)
+	//! Overlap flag bit position (fmt >= 6).
 	constexpr unsigned OVERLAP_FLAG_BIT_POS  = 3;
 	constexpr unsigned OVERLAP_FLAG_BIT_MASK = 1 << OVERLAP_FLAG_BIT_POS;
 
-	/// Header part of a LAS Extended VLR
-	///
-	/// In a LAS file, EVLRs are stored after the points.
-	///
-	/// We need this struct as Waveform data can be stored inside EVLRs.
+	/**
+	 * @brief Extended VLR header for LAS files.
+	 *
+	 * EVLRs are stored after points in LAS files.
+	 * Used for waveform data storage.
+	 */
 	struct EvlrHeader
 	{
-		static constexpr size_t SIZE             = 60;
-		static constexpr size_t USER_ID_SIZE     = 16;
-		static constexpr size_t DESCRIPTION_SIZE = 32;
+		static constexpr size_t SIZE             = 60; //!< Header size.
+		static constexpr size_t USER_ID_SIZE     = 16; //!< User ID size.
+		static constexpr size_t DESCRIPTION_SIZE = 32; //!< Description size.
 
-		char     userID[USER_ID_SIZE];
+		//! User ID.
+		char userID[USER_ID_SIZE];
+
+		//! Record ID.
 		uint16_t recordID{0};
-		uint64_t recordLength{0};
-		char     description[DESCRIPTION_SIZE];
 
+		//! Record length.
+		uint64_t recordLength{0};
+
+		//! Description.
+		char description[DESCRIPTION_SIZE];
+
+		//! Default constructor.
 		EvlrHeader() = default;
 
+		/**
+		 * @brief Create waveform EVLR header.
+		 *
+		 * @return Waveform header.
+		 */
 		static EvlrHeader Waveform();
 
+		/**
+		 * @brief Check if waveform data packets.
+		 *
+		 * @return true if waveform.
+		 */
 		bool isWaveFormDataPackets() const;
 
+		/**
+		 * @brief Check if COPC entry.
+		 *
+		 * @return true if COPC.
+		 */
 		bool isCOPCEntry() const;
 
+		//! Serialization operators.
 		friend QDataStream& operator>>(QDataStream& stream, EvlrHeader& hdr);
 		friend QDataStream& operator<<(QDataStream& stream, const EvlrHeader& hdr);
 	};
 
-	/// Returns the size for the given point format id
-	///
-	/// Returns a size of 0 if the point format does not exist or is not handled
-	///
-	/// \param pointFormat the point format
-	///
+	/**
+	 * @brief Get point format size.
+	 *
+	 * @param[in] pointFormat Point format ID.
+	 *
+	 * @return Size in bytes, 0 if unsupported.
+	 */
 	uint16_t PointFormatSize(unsigned pointFormat);
 
-	/// Returns the header size for the given minor version of the standard used
+	/**
+	 * @brief Get header size for version.
+	 *
+	 * @param[in] versionMinor Version minor.
+	 *
+	 * @return Header size.
+	 */
 	uint16_t HeaderSize(unsigned versionMinor);
 
-	/// Returns whether the point format supports GPS Time
+	/**
+	 * @brief Check if format supports GPS time.
+	 *
+	 * @param[in] pointFormatId Point format.
+	 *
+	 * @return true if supported.
+	 */
 	inline bool HasGpsTime(unsigned pointFormatId)
 	{
 		return pointFormatId == 1
 		       || pointFormatId >= 3;
 	}
 
-	/// Returns whether the point format supports RGB
+	/**
+	 * @brief Check if format supports RGB.
+	 *
+	 * @param[in] pointFormatId Point format.
+	 *
+	 * @return true if supported.
+	 */
 	inline bool HasRGB(unsigned pointFormatId)
 	{
 		return pointFormatId == 2
@@ -188,7 +277,13 @@ namespace LasDetails
 		       || pointFormatId == 10;
 	}
 
-	/// Returns whether the point format supports Waveforms
+	/**
+	 * @brief Check if format supports waveforms.
+	 *
+	 * @param[in] pointFormatId Point format.
+	 *
+	 * @return true if supported.
+	 */
 	inline bool HasWaveform(unsigned pointFormatId)
 	{
 		return pointFormatId == 4
@@ -196,46 +291,88 @@ namespace LasDetails
 		       || pointFormatId >= 9;
 	}
 
-	/// Returns whether the point format supports Near Infrared
+	/**
+	 * @brief Check if format supports NIR.
+	 *
+	 * @param[in] pointFormatId Point format.
+	 *
+	 * @return true if supported.
+	 */
 	inline bool HasNearInfrared(unsigned pointFormatId)
 	{
 		return pointFormatId == 8
 		       || pointFormatId == 10;
 	}
 
-	/// Returns the number of bytes the VLRs amounts to.
-	/// This includes the headers of each VLRs.
+	/**
+	 * @brief Get VLRs total size.
+	 *
+	 * @param[in] vlrs VLR array.
+	 * @param[in] numVlrs Number of VLRs.
+	 *
+	 * @return Total bytes.
+	 */
 	unsigned SizeOfVlrs(const laszip_vlr_struct* vlrs, unsigned numVlrs);
 
-	/// Returns whether the vlr is the vlr for/of LASzip compression.
+	/**
+	 * @brief Check if VLR is LASzip compression.
+	 *
+	 * @param[in] vlr VLR.
+	 *
+	 * @return true if LASzip.
+	 */
 	bool IsLaszipVlr(const laszip_vlr_struct&);
 
-	/// Returns whether the vlr describes extra bytes.
+	/**
+	 * @brief Check if VLR is extra bytes.
+	 *
+	 * @param[in] vlr VLR.
+	 *
+	 * @return true if extra bytes.
+	 */
 	bool IsExtraBytesVlr(const laszip_vlr_struct&);
 
-	/// Returns point the formats available for the given version.
-	///
-	/// If the version does not exists or is not supported a nullptr is returned.
-	///
-	/// \param version version string, must be "major.minor" e.g. "1.2"
+	/**
+	 * @brief Get available point formats.
+	 *
+	 * @param[in] version Version string (e.g., "1.2").
+	 *
+	 * @return Point format list.
+	 */
 	const std::vector<unsigned>& PointFormatsAvailableForVersion(const QString& version);
 
+	/**
+	 * @brief Get available versions.
+	 *
+	 * @return Version array.
+	 */
 	const std::array<const char*, 3>& AvailableVersions();
 
-	/// See `SelectBestVersion`
+	/**
+	 * @brief LAS version selection.
+	 */
 	struct LasVersion
 	{
-		int pointFormat  = 3;
-		int minorVersion = 2;
+		int pointFormat  = 3; //!< Point format.
+		int minorVersion = 2; //!< Version minor.
 	};
 
-	/// This function looks into the point cloud
-	/// and returns a LAS version + point format
-	/// that best matches what the point cloud contains.
-	/// If a previous minor version is provided, this method won't downgrade it
+	/**
+	 * @brief Select best version for cloud.
+	 *
+	 * @param[in] cloud Point cloud.
+	 * @param[in] previousMinorVersion Previous version.
+	 *
+	 * @return Best version.
+	 */
 	LasVersion SelectBestVersion(const ccPointCloud& cloud, int previousMinorVersion = 0);
 
-	/// Clones the content of the `src` vlr into the `dst` vlr.
+	/**
+	 * @brief Clone VLR content.
+	 *
+	 * @param[in] src Source VLR.
+	 * @param[out] dst Destination VLR.
+	 */
 	void CloneVlrInto(const laszip_vlr_struct& src, laszip_vlr_struct& dst);
 
 } // namespace LasDetails
