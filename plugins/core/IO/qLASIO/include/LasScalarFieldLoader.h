f4b8489b6215731b78d7e87b35e6c309db61ba31
Mavis <Mavis@mavis.local>
2026-08-20 13:44:19 +0100

    docs: enhance LasScalarFieldLoader.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasScalarFieldLoader.h
+++ plugins/core/IO/qLASIO/include/LasScalarFieldLoader.h
@@ -1,3 +1,19 @@
+/**
+ * @file LasScalarFieldLoader.h
+ *
+ * @brief LAS scalar field loader.
+ *
+ * @details Loads LAS dimensions into CloudCompare scalar fields.
+ *
+ * ## Loading Process
+ *
+ * 1. Create loader with field mappings
+ * 2. For each point, call handleScalarFields()
+ * 3. Scalar values are stored in CloudCompare scalar fields
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,7 +21,6 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 of the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
@@ -14,7 +29,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 #include "LasDetails.h"
@@ -32,98 +47,203 @@
 #include <laszip/laszip_api.h>
 
 /**
- * @class LasScalarFieldLoader
+ * @brief LAS scalar field loader.
  *
- * @brief LAS scalar field loader
- *
- * Load LAS dimensions into CloudCompare scalar fields.
+ * Loads LAS dimensions into CloudCompare scalar fields.
  */
 class LasScalarFieldLoader
 {
   public:
+	/**
+	 * @brief Create loader.
+	 *
+	 * @param[in,out] standardScalarFields Standard field mappings.
+	 * @param[in,out] extraScalarFields Extra field mappings.
+	 * @param[in,out] pointCloud Point cloud.
+	 */
 	LasScalarFieldLoader(std::vector<LasScalarField>&      standardScalarFields,
-	                     std::vector<LasExtraScalarField>& extraScalarFields,
-	                     ccPointCloud&                     pointCloud);
+	                    std::vector<LasExtraScalarField>& extraScalarFields,
+	                    ccPointCloud&                     pointCloud);
 
+	/**
+	 * @brief Handle scalar fields for current point.
+	 *
+	 * @param[in,out] pointCloud Point cloud.
+	 * @param[in] currentPoint Current LAS point.
+	 *
+	 * @return Error code.
+	 */
 	CC_FILE_ERROR handleScalarFields(ccPointCloud& pointCloud, const laszip_point& currentPoint);
 
-	/// Parses the extra scalar field described by extraField, from currentPoint, into outputValues
+	/**
+	 * @brief Parse extra scalar field.
+	 *
+	 * @param[in] extraField Extra field definition.
+	 * @param[in] currentPoint Current LAS point.
+	 * @param[out] outputValues Output values.
+	 *
+	 * @return Error code.
+	 */
 	CC_FILE_ERROR parseExtraScalarField(const LasExtraScalarField& extraField, const laszip_point& currentPoint, ScalarType outputValues[3]);
 
-	/// In LAS files, the red, green and blue channels are normal LAS fields,
-	/// however in CloudCompare RGB is handled differently.
+	/**
+	 * @brief Handle RGB values.
+	 *
+	 * @param[in,out] pointCloud Point cloud.
+	 * @param[in] currentPoint Current LAS point.
+	 *
+	 * @return Error code.
+	 *
+	 * @note Converts LAS RGB to CloudCompare color.
+	 */
 	CC_FILE_ERROR handleRGBValue(ccPointCloud& pointCloud, const laszip_point& currentPoint);
 
+	/**
+	 * @brief Handle extra scalar fields.
+	 *
+	 * @param[in] currentPoint Current LAS point.
+	 *
+	 * @return Error code.
+	 */
 	CC_FILE_ERROR handleExtraScalarFields(const laszip_point& currentPoint);
 
+	/**
+	 * @brief Set ignore fields with default values.
+	 *
+	 * @param[in] state Ignore flag.
+	 */
 	inline void setIgnoreFieldsWithDefaultValues(bool state)
 	{
 		m_ignoreFieldsWithDefaultValues = state;
 	}
 
+	/**
+	 * @brief Set force 8-bit RGB mode.
+	 *
+	 * @param[in] state Force flag.
+	 */
 	inline void setForce8bitRgbMode(bool state)
 	{
 		m_force8bitRgbMode = state;
 	}
 
-	/// Sets whether the classification field should be decomposed into
-	/// the classification, synthetic flag, key_point flag, withheld flag.
-	///
-	/// Only applies to point format <= 5 (ie field Classification, not ExtendedClassification)
+	/**
+	 * @brief Set decompose classification.
+	 *
+	 * @param[in] state Decompose flag.
+	 *
+	 * @note Only for point format <= 5.
+	 */
 	inline void setDecomposeClassification(bool state)
 	{
 		m_decomposeClassification = state;
 	}
 
+	/**
+	 * @brief Get standard fields.
+	 *
+	 * @return Standard scalar fields.
+	 */
 	inline const std::vector<LasScalarField>& standardFields() const
 	{
 		return m_standardFields;
 	}
 
+	/**
+	 * @brief Get extra fields.
+	 *
+	 * @return Extra scalar fields.
+	 */
 	inline const std::vector<LasExtraScalarField>& extraFields() const
 	{
 		return m_extraScalarFields;
 	}
 
   private:
-	/// Handles loading of LAS value into the scalar field that will be part
-	/// of the pointCloud.
-	///
-	/// sfInfo: Info about the current scalar field we are loading the value into
-	/// pointCloud: The point cloud where the scalar field will be loaded into
-	/// currentValue: The current value of the LAS field we are loading.
+	/**
+	 * @brief Handle single scalar field.
+	 *
+	 * @tparam T Value type.
+	 * @param[in,out] sfInfo Scalar field info.
+	 * @param[in,out] pointCloud Point cloud.
+	 * @param[in] currentValue Current value.
+	 *
+	 * @return Error code.
+	 */
 	template <typename T>
 	CC_FILE_ERROR handleScalarField(LasScalarField& sfInfo, ccPointCloud& pointCloud, T currentValue);
 
-	/// creates the ccScalarFields that correspond to the LAS extra dimensions
+	/**
+	 * @brief Create scalar fields for extra bytes.
+	 *
+	 * @param[in,out] pointCloud Point cloud.
+	 *
+	 * @return true if successful.
+	 */
 	bool createScalarFieldsForExtraBytes(ccPointCloud& pointCloud);
 
-	/// Re-interprets the bytes from the source as a value of type `T`
-	/// and then cast the resulting value from the type T to ScalarType.
+	/**
+	 * @brief Parse value of type.
+	 *
+	 * @tparam T Source type.
+	 * @param[in] source Source bytes.
+	 *
+	 * @return Parsed value as ScalarType.
+	 */
 	template <typename T>
 	static ScalarType ParseValueOfType(uint8_t* source);
 
-	/// Re-interprets the bytes from the source as a value of type `T`
-	/// and then cast the resulting value from the type T to a value of type `V`.
+	/**
+	 * @brief Parse and convert value type.
+	 *
+	 * @tparam T Source type.
+	 * @tparam V Destination type.
+	 * @param[in] source Source bytes.
+	 *
+	 * @return Converted value.
+	 */
 	template <typename T, typename V>
 	static V ParseValueOfTypeAs(const uint8_t* source);
 
-	/// Loads the values for the LAS extra field of the current point from the dataStart source.
-	///
-	/// The loaded values are stored into the member variable `rawValues`
+	/**
+	 * @brief Parse raw values from point.
+	 *
+	 * @param[in] extraField Extra field.
+	 * @param[in] dataStart Data start pointer.
+	 */
 	void parseRawValues(const LasExtraScalarField& extraField, const uint8_t* dataStart);
 
+	/**
+	 * @brief Handle options for extra field.
+	 *
+	 * @tparam T Value type.
+	 * @param[in] extraField Extra field.
+	 * @param[in] inputValues Input values.
+	 * @param[out] outputValues Output values.
+	 */
 	template <typename T>
 	void handleOptionsFor(const LasExtraScalarField& extraField, T inputValues[3], ScalarType outputValues[3]);
 
   private:
-	bool                              m_force8bitRgbMode{false};
-	bool                              m_decomposeClassification{true};
-	bool                              m_ignoreFieldsWithDefaultValues{true};
-	unsigned char                     m_colorCompShift{0};
-	std::vector<LasScalarField>&      m_standardFields;
+	//! Force 8-bit RGB.
+	bool m_force8bitRgbMode{false};
+
+	//! Decompose classification.
+	bool m_decomposeClassification{true};
+
+	//! Ignore default values.
+	bool m_ignoreFieldsWithDefaultValues{true};
+
+	//! Color component shift.
+	unsigned char m_colorCompShift{0};
+
+	//! Standard fields.
+	std::vector<LasScalarField>& m_standardFields;
+
+	//! Extra fields.
 	std::vector<LasExtraScalarField>& m_extraScalarFields;
 
+	//! Raw values union.
 	union
 	{
 		uint64_t unsignedValues[LasExtraScalarField::MAX_DIM_SIZE];
