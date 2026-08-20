496753707b7eb4353b880ee8c66bec582073bc21
Mavis <Mavis@mavis.local>
2026-08-20 13:45:34 +0100

    docs: enhance LasTiler.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasTiler.h
+++ plugins/core/IO/qLASIO/include/LasTiler.h
@@ -1,3 +1,19 @@
+/**
+ * @file LasTiler.h
+ *
+ * @brief LAS tiling utilities.
+ *
+ * @details Functions for tiling LAS/LAZ files into grid-based tiles.
+ *
+ * ## Tiling Dimensions
+ *
+ * - XY: Tile in X-Y plane
+ * - XZ: Tile in X-Z plane
+ * - YZ: Tile in Y-Z plane
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,7 +21,6 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 of the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
@@ -14,7 +29,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 #include <FileIOFilter.h>
@@ -22,24 +37,37 @@
 #include <laszip/laszip_api.h>
 
 /**
- * @enum LasTilingDimensions
- *
- * @brief Tiling dimensions
+ * @brief Tiling dimension enumeration.
  */
 enum class LasTilingDimensions
 {
-	XY = 0,
-	XZ = 1,
-	YZ = 2,
+	XY = 0, //!< Tile in X-Y plane.
+	XZ = 1, //!< Tile in X-Z plane.
+	YZ = 2, //!< Tile in Y-Z plane.
 };
 
+/**
+ * @brief LAS tiling options.
+ */
 struct LasTilingOptions final
 {
-	QString             outputDir;
-	LasTilingDimensions dims      = LasTilingDimensions::XY;
-	unsigned            numTiles0 = 0;
-	unsigned            numTiles1 = 0;
+	//! Output directory.
+	QString outputDir;
 
+	//! Tiling dimensions.
+	LasTilingDimensions dims = LasTilingDimensions::XY;
+
+	//! Number of tiles in dimension 0.
+	unsigned numTiles0 = 0;
+
+	//! Number of tiles in dimension 1.
+	unsigned numTiles1 = 0;
+
+	/**
+	 * @brief Get index 0.
+	 *
+	 * @return Index based on dimensions.
+	 */
 	inline size_t index0() const
 	{
 		switch (dims)
@@ -54,6 +82,11 @@ struct LasTilingOptions final
 		return 0;
 	}
 
+	/**
+	 * @brief Get index 1.
+	 *
+	 * @return Index based on dimensions.
+	 */
 	inline size_t index1() const
 	{
 		switch (dims)
@@ -69,7 +102,15 @@ struct LasTilingOptions final
 	}
 };
 
-/// Tiles the cloud that the reader reads into a grid described by the options.
-///
-/// This takes ownership of the reader and takes care of closing and deleting it
+/**
+ * @brief Tile a LAS reader into a grid.
+ *
+ * Takes ownership of the reader and handles closing/deleting.
+ *
+ * @param[in] laszipReader LASzip reader.
+ * @param[in] originName Original file name.
+ * @param[in] options Tiling options.
+ *
+ * @return Error code.
+ */
 CC_FILE_ERROR TileLasReader(laszip_POINTER laszipReader, const QString& originName, const LasTilingOptions& options);
