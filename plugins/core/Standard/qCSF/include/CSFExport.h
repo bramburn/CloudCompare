bce855b0f7848f31b335a41a08286263f0fc9a47
Mavis <Mavis@mavis.local>
2026-08-20 13:58:46 +0100

    docs: enhance CSFExport.h Doxygen documentation

--- plugins/core/Standard/qCSF/include/CSFExport.h
+++ plugins/core/Standard/qCSF/include/CSFExport.h
@@ -1,3 +1,11 @@
+/**
+ * @file CSFExport.h
+ *
+ * @brief CSF library export macros.
+ *
+ * @details DLL export/import macros for CSF library.
+ */
+
 #pragma once
 
 // CSF_LIB_EXPORTS is set when compiling CSF_lib (the static algorithm lib).
