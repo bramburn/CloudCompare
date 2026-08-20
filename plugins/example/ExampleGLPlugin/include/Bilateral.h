23734b0fa5bb09b5172297f44d44ea1e1fcecca0
Mavis <Mavis@mavis.local>
2026-08-20 14:21:59 +0100

    docs: enhance Bilateral.h Doxygen documentation

--- plugins/example/ExampleGLPlugin/include/Bilateral.h
+++ plugins/example/ExampleGLPlugin/include/Bilateral.h
@@ -1,3 +1,11 @@
+/**
+ * @file Bilateral.h
+ *
+ * @brief Bilateral filter header.
+ *
+ * @details Example GL filter function declarations.
+ */
+
 // Example of a GL filter for the GL filter plugin
 
 #pragma once
