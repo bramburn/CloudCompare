e4405e271a5894760f210baba6b65675440c06b4
Mavis <Mavis@mavis.local>
2026-08-20 13:41:03 +0100

    docs: enhance LasMetadata.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasMetadata.h
+++ plugins/core/IO/qLASIO/include/LasMetadata.h
@@ -1,3 +1,23 @@
+/**
+ * @file LasMetadata.h
+ *
+ * @brief LAS metadata handling utilities.
+ *
+ * @details Functions for saving and loading LAS metadata
+ * to/from CloudCompare point clouds.
+ *
+ * ## Metadata Keys
+ *
+ * - Scale factors (X, Y, Z)
+ * - Offset values (X, Y, Z)
+ * - Version (major, minor)
+ * - Point format
+ * - Global encoding
+ * - VLRs (Variable Length Records)
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,7 +25,6 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 of the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
@@ -14,7 +33,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 struct laszip_header;
@@ -30,34 +49,105 @@ class ccPointCloud;
 #include <laszip/laszip_api.h>
 
 /**
- * @namespace LasMetadata
- *
- * @brief LAS metadata handling
- *
- * Functions for saving and loading LAS metadata.
+ * @brief LAS metadata handling namespace.
  */
 namespace LasMetadata
 {
+	//! X scale metadata key.
 	constexpr const char X_SCALE[]           = "LAS.scale.x";
+	//! Y scale metadata key.
 	constexpr const char Y_SCALE[]           = "LAS.scale.y";
+	//! Z scale metadata key.
 	constexpr const char Z_SCALE[]           = "LAS.scale.z";
+	//! X offset metadata key.
 	constexpr const char X_OFFSET[]          = "LAS.offset.x";
+	//! Y offset metadata key.
 	constexpr const char Y_OFFSET[]          = "LAS.offset.y";
+	//! Z offset metadata key.
 	constexpr const char Z_OFFSET[]          = "LAS.offset.z";
+	//! Version major metadata key.
 	constexpr const char VERSION_MAJOR[]     = "LAS.version.major";
+	//! Version minor metadata key.
 	constexpr const char VERSION_MINOR[]     = "LAS.version.minor";
+	//! Point format metadata key.
 	constexpr const char POINT_FORMAT[]      = "LAS.point_format";
+	//! Global encoding metadata key.
 	constexpr const char GLOBAL_ENCODING[]   = "LAS.global_encoding";
+	//! Project UUID metadata key.
 	constexpr const char PROJECT_UUID[]      = "LAS.project_uuid";
+	//! System identifier metadata key.
 	constexpr const char SYSTEM_IDENTIFIER[] = "LAS.system_identifier";
+	//! VLRs metadata key.
 	constexpr const char VLRS[]              = "LAS.variableLengthRecords";
+	//! Extra fields metadata key.
 	constexpr const char EXTRA_FIELDS[]      = "LAS.extra_fields";
 
+	/**
+	 * @brief Save metadata into point cloud.
+	 *
+	 * @param[in] header LAS header.
+	 * @param[out] pointCloud Point cloud.
+	 * @param[in] extraScalarFields Extra scalar fields.
+	 */
 	void SaveMetadataInto(const laszip_header& header, ccPointCloud& pointCloud, const std::vector<LasExtraScalarField>& extraScalarFields);
+
+	/**
+	 * @brief Load VLRs from point cloud.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] vlr VLR container.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadVlrs(const ccPointCloud& pointCloud, LasVlr& vlr);
+
+	/**
+	 * @brief Load scale from point cloud.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] scale Scale vector.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadScaleFrom(const ccPointCloud& pointCloud, CCVector3d& scale);
+
+	/**
+	 * @brief Load offset from point cloud.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] offset Offset vector.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadOffsetFrom(const ccPointCloud& pointCloud, CCVector3d& offset);
+
+	/**
+	 * @brief Load LAS version from point cloud.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] version LAS version.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadLasVersionFrom(const ccPointCloud& pointCloud, LasDetails::LasVersion& version);
+
+	/**
+	 * @brief Load global encoding from point cloud.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] outGlobalEncoding Global encoding.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadGlobalEncoding(const ccPointCloud& pointCloud, uint16_t& outGlobalEncoding);
+
+	/**
+	 * @brief Load project UUID into header.
+	 *
+	 * @param[in] pointCloud Point cloud.
+	 * @param[out] header LAS header.
+	 *
+	 * @return true if successful.
+	 */
 	bool LoadProjectUUID(const ccPointCloud& pointCloud, laszip_header& header);
 } // namespace LasMetadata
