3a09d76d99d287ce46ff8bc8631199be052ce45c
Mavis <Mavis@mavis.local>
2026-08-20 13:43:30 +0100

    docs: enhance LasSaver.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasSaver.h
+++ plugins/core/IO/qLASIO/include/LasSaver.h
@@ -1,3 +1,30 @@
+/**
+ * @file LasSaver.h
+ *
+ * @brief LAS file saver class.
+ *
+ * @details Saves point clouds to LAS/LAZ files.
+ *
+ * ## Usage
+ *
+ * @code
+ * LasSaver::Parameters params;
+ * params.versionMajor = 1;
+ * params.versionMinor = 4;
+ * params.pointFormat = 6;
+ * params.shouldSaveRGB = true;
+ * // ... configure fields ...
+ *
+ * LasSaver saver(cloud, params);
+ * saver.open("output.las");
+ * for (size_t i = 0; i < cloud.size(); ++i) {
+ *     saver.saveNextPoint();
+ * }
+ * @endcode
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,7 +32,6 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
 // #  the Free Software Foundation; version 2 of the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
@@ -14,7 +40,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 #include "LasScalarFieldSaver.h"
@@ -32,54 +58,128 @@
 class ccPointCloud;
 
 /**
- * @class LasSaver
+ * @brief LAS file saver.
  *
- * @brief LAS file saver
- *
- * Save point clouds to LAS/LAZ files.
+ * Saves point clouds to LAS/LAZ files.
  */
 class LasSaver
 {
   public:
+	/**
+	 * @brief Save parameters.
+	 */
 	struct Parameters
 	{
-		std::vector<LasScalarField>      standardFields;
+		//! Standard scalar fields.
+		std::vector<LasScalarField> standardFields;
+
+		//! Extra scalar fields.
 		std::vector<LasExtraScalarField> extraFields;
-		bool                             shouldSaveRGB{false};
-		bool                             shouldSaveWaveform{false};
-		bool                             shouldSaveNormalsAsExtraScalarField{false};
-		uint8_t                          versionMajor{1};
-		uint8_t                          versionMinor{0};
-		uint8_t                          pointFormat{0};
-		CCVector3d                       lasScale;
-		CCVector3d                       lasOffset;
+
+		//! Save RGB colors.
+		bool shouldSaveRGB{false};
+
+		//! Save waveforms.
+		bool shouldSaveWaveform{false};
+
+		//! Save normals as extra scalar field.
+		bool shouldSaveNormalsAsExtraScalarField{false};
+
+		//! Version major.
+		uint8_t versionMajor{1};
+
+		//! Version minor.
+		uint8_t versionMinor{0};
+
+		//! Point format.
+		uint8_t pointFormat{0};
+
+		//! LAS scale.
+		CCVector3d lasScale;
+
+		//! LAS offset.
+		CCVector3d lasOffset;
 	};
 
+	/**
+	 * @brief Create saver.
+	 *
+	 * @param[in] cloud Point cloud to save.
+	 * @param[in] parameters Save parameters.
+	 */
 	LasSaver(ccPointCloud& cloud, Parameters parameters);
+
+	/**
+	 * @brief Destructor.
+	 */
 	~LasSaver() noexcept;
 
+	/**
+	 * @brief Open file for writing.
+	 *
+	 * @param[in] filePath Output file path.
+	 *
+	 * @return Error code.
+	 */
 	CC_FILE_ERROR open(const QString filePath);
 
+	/**
+	 * @brief Save next point.
+	 *
+	 * @return Error code.
+	 */
 	CC_FILE_ERROR saveNextPoint();
 
+	/**
+	 * @brief Check if waveforms can be saved.
+	 *
+	 * @return true if supported.
+	 */
 	bool canSaveWaveforms() const;
 
+	/**
+	 * @brief Get last error message.
+	 *
+	 * @return Error string.
+	 */
 	QString getLastError() const;
 
   private:
+	/**
+	 * @brief Initialize LASzip header.
+	 *
+	 * @param[in] parameters Parameters.
+	 */
 	void initLaszipHeader(const Parameters& parameters);
 
   private:
-	unsigned                          m_currentPointIndex{0};
-	ccPointCloud&                     m_cloudToSave;
-	laszip_header                     m_laszipHeader{};
-	laszip_POINTER                    m_laszipWriter{nullptr};
-	LasScalarFieldSaver               m_fieldsSaver;
-	bool                              m_shouldSaveRGB{false};
+	//! Current point index.
+	unsigned m_currentPointIndex{0};
+
+	//! Point cloud.
+	ccPointCloud& m_cloudToSave;
+
+	//! LASzip header.
+	laszip_header m_laszipHeader{};
+
+	//! LASzip writer.
+	laszip_POINTER m_laszipWriter{nullptr};
+
+	//! Fields saver.
+	LasScalarFieldSaver m_fieldsSaver;
+
+	//! Save RGB flag.
+	bool m_shouldSaveRGB{false};
+
+	//! Waveform saver.
 	std::unique_ptr<LasWaveformSaver> m_waveformSaver{nullptr};
-	laszip_point*                     m_laszipPoint{nullptr};
-	int                               m_originallySelectedScalarField = -1;
-	// contains for the x, y, z dims of the normals, whether it was temporarily
-	// exported to a scalar field. If true, then we have to remove the temporary sf.
+
+	//! LASzip point.
+	laszip_point* m_laszipPoint{nullptr};
+
+	//! Original scalar field index.
+	int m_originallySelectedScalarField = -1;
+
+	//! Normal dim temporary export flags.
 	bool m_normalDimWasTemporarillyExported[3] = {false, false, false};
 };
