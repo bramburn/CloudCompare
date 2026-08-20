65c5dd6678377c14c9636213f90e63d1086ce882
Mavis <Mavis@mavis.local>
2026-08-20 13:43:03 +0100

    docs: enhance LasScalarField.h Doxygen documentation

--- plugins/core/IO/qLASIO/include/LasScalarField.h
+++ plugins/core/IO/qLASIO/include/LasScalarField.h
@@ -1,3 +1,28 @@
+/**
+ * @file LasScalarField.h
+ *
+ * @brief LAS scalar field mapping structures.
+ *
+ * @details Links LAS standard fields to CloudCompare scalar fields
+ * for reading and writing LAS files.
+ *
+ * ## Scalar Fields
+ *
+ * LAS standard scalar fields include:
+ * - Intensity
+ * - Return Number
+ * - Classification
+ * - GPS Time
+ * - Scan Angle
+ * - And more...
+ *
+ * ## Extended Fields (LAS 1.4+)
+ *
+ * Extended fields have higher precision and additional data.
+ *
+ * @author Thomas Montaigu
+ */
+
 #pragma once
 
 // ##########################################################################
@@ -5,8 +30,7 @@
 // #                CLOUDCOMPARE PLUGIN: LAS-IO Plugin                      #
 // #                                                                        #
 // #  This program is free software; you can redistribute it and/or modify  #
-// #  it under the terms of the GNU General Public License as published by  #
-// #  the Free Software Foundation; version 2 of the License.               #
+// #  the Free Software Foundation; version 2 or the License.               #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
 // #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
@@ -14,7 +38,7 @@
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
 // #                   COPYRIGHT: Thomas Montaigu                           #
-// #                                                                        #
+// #                                                                        //
 // ##########################################################################
 
 // CCCoreLib
@@ -26,50 +50,54 @@
 #include <cstdint>
 
 /**
- * @struct LasScalarField
- *
- * @brief LAS scalar field mapping
+ * @brief LAS scalar field mapping.
  *
  * Links a LAS standard field to a CloudCompare scalar field.
  */
 struct LasScalarField
 {
-	/// Enum used to uniquely identify LAS fields
-	/// with a clear distinction between 'normal' and 'extended' fields
-	/// which may have the same name (e.g. Classification)
+	/**
+	 * @brief LAS field ID enumeration.
+	 */
 	enum Id
 	{
-		Intensity       = 0,
-		ReturnNumber    = 1,
-		NumberOfReturns = 2,
-		ScanDirectionFlag,
-		EdgeOfFlightLine,
-		Classification,
-		SyntheticFlag,
-		KeypointFlag,
-		WithheldFlag,
-		ScanAngleRank,
-		UserData,
-		PointSourceId,
-		GpsTime,
+		Intensity       = 0, //!< Return intensity.
+		ReturnNumber    = 1, //!< Return number.
+		NumberOfReturns = 2, //!< Number of returns.
+		ScanDirectionFlag,   //!< Scan direction.
+		EdgeOfFlightLine,    //!< Edge of flight line.
+		Classification,      //!< Classification.
+		SyntheticFlag,        //!< Synthetic flag.
+		KeypointFlag,        //!< Keypoint flag.
+		WithheldFlag,        //!< Withheld flag.
+		ScanAngleRank,       //!< Scan angle rank.
+		UserData,            //!< User data.
+		PointSourceId,       //!< Point source ID.
+		GpsTime,             //!< GPS time.
 		// Extended (LAS 1.4)
-		ExtendedScanAngle,
-		ExtendedScannerChannel,
-		OverlapFlag,
-		ExtendedClassification,
-		ExtendedReturnNumber,
-		ExtendedNumberOfReturns,
-		NearInfrared
+		ExtendedScanAngle,            //!< Extended scan angle.
+		ExtendedScannerChannel,      //!< Scanner channel.
+		OverlapFlag,                //!< Overlap flag.
+		ExtendedClassification,       //!< Extended classification.
+		ExtendedReturnNumber,        //!< Extended return number.
+		ExtendedNumberOfReturns,     //!< Extended number of returns.
+		NearInfrared                //!< Near infrared.
 	};
 
-	/// Represents an inclusive range of value from a minimum to a maximum.
-	///
-	/// This is used to be able to detect and inform users from possible data loss.
+	/**
+	 * @brief Value range structure.
+	 *
+	 * Represents an inclusive range for detecting data loss.
+	 */
 	struct Range
 	{
-		/// Creates a new Range given the min and max value.
-		///
-		/// These values are converted to ScalarType
+		/**
+		 * @brief Create range from min/max.
+		 *
+		 * @tparam T Numeric type.
+		 * @param[in] minValue Minimum value.
+		 * @param[in] maxValue Maximum value.
+		 */
 		template <class T>
 		constexpr Range(T minValue, T maxValue)
 		    : min(static_cast<ScalarType>(minValue))
@@ -77,55 +105,105 @@ struct LasScalarField
 		{
 		}
 
-		/// Shortcut to create a Range from a native type like uint16_t
+		/**
+		 * @brief Create range for type.
+		 *
+		 * @tparam T Numeric type.
+		 *
+		 * @return Range with type min/max.
+		 */
 		template <class T>
 		static constexpr Range ForType()
 		{
 			return Range(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 		}
 
+		/**
+		 * @brief Create range for bit count.
+		 *
+		 * @param[in] numBits Number of bits.
+		 *
+		 * @return Range for bit count.
+		 */
 		static Range ForBitCount(uint8_t numBits)
 		{
 			return Range(0, ((1 << static_cast<uint32_t>(numBits)) - 1));
 		}
 
+		//! Minimum value.
 		ScalarType min = 0;
+		//! Maximum value.
 		ScalarType max = 0;
 	};
 
-  public: // Methods and Constructors
+  public: // constructors
+	//! Deleted default constructor.
 	LasScalarField() = delete;
 
+	/**
+	 * @brief Create scalar field mapping.
+	 *
+	 * @param[in] id LAS field ID.
+	 * @param[in] sf CloudCompare scalar field.
+	 */
 	explicit LasScalarField(LasScalarField::Id id, ccScalarField* sf = nullptr);
 
+	/**
+	 * @brief Get LAS field name.
+	 *
+	 * @return Field name.
+	 */
 	const char* name() const;
 
-  public: // Static functions
-	/// Returns the name that correspond to the given LasScalarField::Id variant
+  public: // static functions
+	/**
+	 * @brief Get name from ID.
+	 *
+	 * @param[in] id Field ID.
+	 *
+	 * @return Field name.
+	 */
 	static constexpr const char* NameFromId(LasScalarField::Id id);
-	/// Returns the  LasScalarField::Id variant that correspond to the given name
-	///
-	/// Point Format is needed, as we have different Id for the same name depending on the point format.
-	/// For example: The "Classification" field has a different Id in fmt >= 6 as it allows to store more information.
-	///
-	/// Throws a logic_error if the name does not have a matching id
+
+	/**
+	 * @brief Get ID from name.
+	 *
+	 * @param[in] name Field name.
+	 * @param[in] targetPointFormat Point format version.
+	 *
+	 * @return Field ID.
+	 *
+	 * @throws std::logic_error if not found.
+	 */
 	static LasScalarField::Id IdFromName(const char* name, unsigned targetPointFormat);
-	/// Returns the range of value the given field (ID) supports
+
+	/**
+	 * @brief Get value range for field.
+	 *
+	 * @param[in] id Field ID.
+	 *
+	 * @return Value range.
+	 */
 	static LasScalarField::Range ValueRange(LasScalarField::Id id);
-	/// Returns the scalar fields that correspond the the point format ID
-	/// as per LAS specifications.
+
+	/**
+	 * @brief Get fields for point format.
+	 *
+	 * @param[in] pointFormatId Point format ID.
+	 *
+	 * @return Vector of scalar fields.
+	 */
 	static std::vector<LasScalarField> ForPointFormat(unsigned pointFormatId);
 
-  public: // Members
-	/// The Id of the LAS field this relates to.
+  public: // members
+	//! LAS field ID.
 	Id id;
-	/// Pointer to the 'linked' CloudCompare scalar field.
-	///
-	/// When reading (loading points) values of the LAS field will be stored into the
-	/// scalar field pointed by sf.
-	///
-	/// When writing (saving points) values of the scalar field pointed by sf will
-	/// be stored to the corresponding LAS field (using the Id).
+
+	//! CloudCompare scalar field.
+	//! - Reading: values stored here
+	//! - Writing: values read from here
 	ccScalarField* sf{nullptr};
-	Range          range;
+
+	//! Value range.
+	Range range;
 };
