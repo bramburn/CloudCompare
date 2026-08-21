12c26a7236fa7f3d55765b3d1f8ce35701cfaeeb
Mavis <Mavis@mavis.local>
2026-08-21 13:08:38 +0100

    docs: add Doxygen comments to LasPlugin.cpp
    - Document plugin entry point: registers LasIOFilter
    - Document QMetaType registration: LasVlr QVariant serialization
    - Document getFilters: returns LasIOFilter for I/O

--- plugins/core/IO/qLASIO/src/LasPlugin.cpp
+++ plugins/core/IO/qLASIO/src/LasPlugin.cpp
@@ -15,11 +15,32 @@
 // #                                                                        #
 // ##########################################################################
 
+/**
+ * @file LasPlugin.cpp
+ *
+ * @brief LAS/LAZ I/O plugin entry point
+ *
+ * Registers the LasIOFilter with CloudCompare's file I/O system.
+ * Handles QMetaType registration for LasVlr QVariant serialization.
+ *
+ * ## Plugin Registration
+ *
+ * The plugin registers LasVlr as a Qt metatype so it can be stored
+ * in QVariant (used for round-trip VLR persistence in ccPointCloud metadata).
+ *
+ * @see LasIOFilter for the main filter implementation
+ */
+
 #include "LasPlugin.h"
 
 #include "LasIOFilter.h"
 #include "LasVlr.h"
 
+/**
+ * @brief Construct the LAS/LAZ plugin
+ *
+ * Registers the LasVlr metatype for QVariant serialization.
+ */
 LasPlugin::LasPlugin(QObject* parent)
     : QObject(parent)
     , ccIOPluginInterface(":/CC/plugin/LAS-IO/info.json")
@@ -29,6 +50,11 @@ LasPlugin::LasPlugin(QObject* parent)
 	QMetaType::registerConverter(&LasVlr::toString);
 }
 
+/**
+ * @brief Get the list of file I/O filters
+ *
+ * @return Single-element list containing the LasIOFilter
+ */
 ccIOPluginInterface::FilterList LasPlugin::getFilters()
 {
 	return {
