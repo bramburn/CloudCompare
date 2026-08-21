121d4f1f199823a6777b648e816661c374c7151d
Mavis <Mavis@mavis.local>
2026-08-21 12:58:07 +0100

    docs: add Doxygen comments to ccCylinder.cpp
    - Document cylinder as special case of cone: equal top/bottom radii
    - Document setBottomRadius: enforces invariant (top = bottom)
    - Document clone: uses m_bottomRadius and m_height

--- libs/qCC_db/src/ccCylinder.cpp
+++ libs/qCC_db/src/ccCylinder.cpp
@@ -7,7 +7,7 @@
 // #  the Free Software Foundation; version 2 or later of the License.      #
 // #                                                                        #
 // #  This program is distributed in the hope that it will be useful,       #
-// #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+// #  WITHOUT ANY WARRANTY; without even the implied warranty of            #
 // #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 // #  GNU General Public License for more details.                          #
 // #                                                                        #
@@ -17,6 +17,20 @@
 
 #include "ccCylinder.h"
 
+// ccCylinder::ccCylinder
+/**
+ * @brief Construct a cylinder
+ *
+ * Delegates to ccCone with equal bottom and top radii (r = r, so a cylinder
+ * is a frustum where both end caps have the same radius).
+ *
+ * @param[in] radius Circular cross-section radius
+ * @param[in] height Z extent
+ * @param[in] transMat Optional transformation matrix
+ * @param[in] name Display name
+ * @param[in] precision Angular tessellation steps
+ * @param[in] uniqueID Optional unique ID
+ */
 ccCylinder::ccCylinder(PointCoordinateType radius,
                        PointCoordinateType height,
                        const ccGLMatrix*   transMat /*=nullptr*/,
@@ -27,19 +41,38 @@ ccCylinder::ccCylinder(PointCoordinateType radius,
 {
 }
 
+// ccCylinder::ccCylinder
+/**
+ * @brief Simplified constructor for ccHObject factory
+ *
+ * @param[in] name Display name
+ */
 ccCylinder::ccCylinder(QString name /*=QString("Cylinder")*/)
     : ccCone(name)
 {
 }
 
+// ccCylinder::clone
+/**
+ * @brief Clone this cylinder
+ */
 ccGenericPrimitive* ccCylinder::clone() const
 {
 	return finishCloneJob(new ccCylinder(m_bottomRadius, m_height, &m_transformation, getName(), m_drawPrecision));
 }
 
+// ccCylinder::setBottomRadius
+/**
+ * @brief Set the cylinder radius
+ *
+ * Overrides ccCone::setBottomRadius to enforce the cylinder invariant:
+ * topRadius must always equal bottomRadius. Sets both simultaneously.
+ *
+ * @param[in] radius New radius (applied to both ends)
+ */
 void ccCylinder::setBottomRadius(PointCoordinateType radius)
 {
-	// we set the top radius as well!
+	// Enforce the cylinder invariant: top radius must equal bottom radius
 	m_topRadius = radius;
 	ccCone::setBottomRadius(radius);
 }
